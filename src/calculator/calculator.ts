f1e055e7a6f4f3997ef245296c7e7033ad7dc5f3
Ryan Benson <benson.ryan@gmail.com>
2024-05-27 20:58:30 -0500

    Refactors character sheet to have each hit an entry, removes damage algorithm from calc

--- src/calculator/calculator.ts
+++ src/calculator/calculator.ts
@@ -33,7 +33,7 @@ export function calcDamage(
 ): number {
   // =((D17*B21))*(1+B25)*(100%+B22*B23)*B29*(1-B30)
   // =((talent*attack)) * (1+damage bonus) * (100%+Crit Rate * Crit Damage) * Def modifier * (1-enemy resist)
-  const talentMv = calculateTalentValue(talent);
+  const talentMv = getTalentValue(talent);
   const talentDamageValue = talentMv * attack;
   const damageBonusValue = 1 + damageBonus;
   const critValue = 1 + critRate * critDamage;
@@ -48,24 +48,10 @@ export function calcDamage(
   );
 }
 
-// Converts something like "45.73%*2" into 0.9146
-// Supports talent values without a multi-hit as well
-export function calculateTalentValue(damageString: string): number {
-  let percentagePart = damageString;
-  let multiplierPart = "1";
-  // Extract the percentage part and the multiplier
-  if (damageString.includes("*")) {
-    [percentagePart, multiplierPart] = damageString.split("*");
-  }
-
+export function getTalentValue(talentStringWithPercent: string): number {
   // Remove the '%' sign and convert the percentage part to a decimal
-  const percentage = parseFloat(percentagePart) / 100;
-
-  // Convert the multiplier part to a number
-  const multiplier = parseFloat(multiplierPart);
-
-  // Calculate and return the damage
-  return percentage * multiplier;
+  const talentValue = parseFloat(talentStringWithPercent) / 100;
+  return talentValue;
 }
 
 export function getDefenseModifier(
