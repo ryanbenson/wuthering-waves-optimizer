7783bed7ea3b70e02f464e62cd727211be85280d
Ryan Benson <benson.ryan@gmail.com>
2024-06-01 14:33:06 -0500

    Refactors character data to make it easier

---
+++ src/characters/Calcharo/outro.ts
@@ -0,0 +1,10 @@
+interface OutroSkill {
+  skillDamage1_1: string;
+  skillDamage1_2: string;
+}
+const outroSkillData: OutroSkill = {
+  skillDamage1_1: "195.98%",
+  skillDamage1_2: "391.96%",
+};
+
+export const outroSkill = { ...outroSkillData };
---
+++ src/characters/characters.ts
@@ -0,0 +1,3 @@
+export function getCharactersAvailable(): string[] {
+  return ["Calcharo"];
+}
