c47beed114995a75d9100b9bc9304d902fbb7f29
Ryan Benson <benson.ryan@gmail.com>
2024-06-12 16:17:21 -0500

    Create introAttacks.ts for Changli

---
+++ src/characters/Changli/introAttacks.ts
@@ -0,0 +1,24 @@
+export const introAttacks = {
+  name: "Intro Skill: Obedience of Rules,
+  description:
+    '<div class="skilldescription">Changli appears in mid-air, attacks the target, and enters <span class="Highlight">True Sight</span>, lasting for 12s.</div>',
+  attacks: [
+    {
+      key: "SkillDMG",
+      label: "Skill DMG",
+      talents: {
+        "1": "22.38% + 13.06%*4",
+        "2": "24.22% + 14.13%*4",
+        "3": "26.06% + 15.20%*4",
+        "4": "28.62% + 16.70%*4",
+        "5": "30.46% + 17.77%*4",
+        "6": "32.57% + 19.00%*4",
+        "7": "35.51% + 20.71%*4",
+        "8": "38.44% + 22.43%*4",
+        "9": "41.38% + 24.14%*4",
+        "10": "44.50% + 25.96%*4",
+      },
+      type: "Intro",
+    },
+  ],
+};
