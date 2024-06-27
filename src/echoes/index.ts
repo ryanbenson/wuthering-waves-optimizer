3f4ac8432240fb07e87a5ec1b7a399a06a51e646
Ryan Benson <benson.ryan@gmail.com>
2024-06-27 10:22:09 -0500

    Update echoes to use arrays for modifiers and removes humans from echoes

--- src/echoes/index.ts
+++ src/echoes/index.ts
@@ -19,504 +19,455 @@ export const mainEchoesData: MainEchoes = {
     name: 'Aero Predator',
     image: '/images/echoes/AeroPredator.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   AlteredClangBang: {
     key: 'AlteredClangBang',
     name: 'Altered Clang Bang',
     image: '/images/echoes/AlteredClangBang.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   AlteredLightcrusher: {
     key: 'AlteredLightcrusher',
     name: 'Altered Lightcrusher',
     image: '/images/echoes/AlteredLightcrusher.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   AutopuppetScout: {
     key: 'AutopuppetScout',
     name: 'Autopuppet Scout',
     image: '/images/echoes/AutopuppetScout.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   BellBorneGeochelone: {
     key: 'BellBorneGeochelone',
     name: 'Bell-Borne Geochelone',
     image: '/images/echoes/BellBorneGeochelone.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   Chaserazor: {
     key: 'Chaserazor',
     name: 'Chaserazor',
     image: '/images/echoes/Chaserazor.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   ChasmGuardian: {
     key: 'ChasmGuardian',
     name: 'Chasm Guardian',
     image: '/images/echoes/ChasmGuardian.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   Chirpuff: {
     key: 'Chirpuff',
     name: 'Chirpuff',
     image: '/images/echoes/Chirpuff.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   ClangBang: {
     key: 'ClangBang',
     name: 'Clang Bang',
     image: '/images/echoes/ClangBang.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   Crownless: {
     key: 'Crownless',
     name: 'Crownless',
     image: '/images/echoes/Crownless.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   Cruisewing: {
     key: 'Cruisewing',
     name: 'Cruisewing',
     image: '/images/echoes/Cruisewing.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   CyanFeatheredHeron: {
     key: 'CyanFeatheredHeron',
     name: 'Cyan-Feathered Heron',
     image: '/images/echoes/CyanFeatheredHeron.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   Diamondclaw: {
     key: 'Diamondclaw',
     name: 'Diamondclaw',
     image: '/images/echoes/Diamondclaw.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   Dreamless: {
     key: 'Dreamless',
     name: 'Dreamless',
     image: '/images/echoes/Dreamless.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   DwarfCassowary: {
     key: 'DwarfCassowary',
     name: 'Dwarf Cassowary',
     image: '/images/echoes/DwarfCassowary.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   ElectroPredator: {
     key: 'ElectroPredator',
     name: 'Electro Predator',
     image: '/images/echoes/ElectroPredator.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   Excarat: {
     key: 'Excarat',
     name: 'Excarat',
     image: '/images/echoes/Excarat.png',
     details: ``,
-    modifiers: {}
-  },
-  Exile: {
-    key: 'Exile',
-    name: 'Exile',
-    image: '/images/echoes/Exile.png',
-    details: ``,
-    modifiers: {}
-  },
-  ExileLeader: {
-    key: 'ExileLeader',
-    name: 'Exile Leader',
-    image: '/images/echoes/ExileLeader.png',
-    details: ``,
-    modifiers: {}
-  },
-  ExileTechnician: {
-    key: 'ExileTechnician',
-    name: 'Exile Technician',
-    image: '/images/echoes/ExileTechnician.png',
-    details: ``,
-    modifiers: {}
+    modifiers: []
   },
   FissionJunrock: {
     key: 'FissionJunrock',
     name: 'Fission Junrock',
     image: '/images/echoes/FissionJunrock.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   Flautist: {
     key: 'Flautist',
     name: 'Flautist',
     image: '/images/echoes/Flautist.png',
     details: ``,
-    modifiers: {}
-  },
-  FractsidusCannoneer: {
-    key: 'FractsidusCannoneer',
-    name: 'Fractsidus Cannoneer',
-    image: '/images/echoes/FractsidusCannoneer.png',
-    details: ``,
-    modifiers: {}
-  },
-  FractsidusExecutioner: {
-    key: 'FractsidusExecutioner',
-    name: 'Fractsidus Executioner',
-    image: '/images/echoes/FractsidusExecutioner.png',
-    details: ``,
-    modifiers: {}
-  },
-  FractsidusGunmaster: {
-    key: 'FractsidusGunmaster',
-    name: 'Fractsidus Gunmaster',
-    image: '/images/echoes/FractsidusGunmaster.png',
-    details: ``,
-    modifiers: {}
-  },
-  FractsidusThruster: {
-    key: 'FractsidusThruster',
-    name: 'Fractsidus Thruster',
-    image: '/images/echoes/FractsidusThruster.png',
-    details: ``,
-    modifiers: {}
+    modifiers: []
   },
   FusionDreadmane: {
     key: 'FusionDreadmane',
     name: 'Fusion Dreadmane',
     image: '/images/echoes/FusionDreadmane.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   FusionPrism: {
     key: 'FusionPrism',
     name: 'Fusion Prism',
     image: '/images/echoes/FusionPrism.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   FusionWarrior: {
     key: 'FusionWarrior',
     name: 'Fusion Warrior',
     image: '/images/echoes/FusionWarrior.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   Gulpuff: {
     key: 'Gulpuff',
     name: 'Gulpuff',
     image: '/images/echoes/Gulpuff.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   HavocDreadmane: {
     key: 'HavocDreadmane',
     name: 'Havoc Dreadmane',
     image: '/images/echoes/HavocDreadmane.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   HavocPrism: {
     key: 'HavocPrism',
     name: 'Havoc Prism',
     image: '/images/echoes/HavocPrism.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   HavocWarrior: {
     key: 'HavocWarrior',
     name: 'Havoc Warrior',
     image: '/images/echoes/HavocWarrior.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   Hoartoise: {
     key: 'Hoartoise',
     name: 'Hoartoise',
     image: '/images/echoes/Hoartoise.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   Hoochief: {
     key: 'Hoochief',
     name: 'Hoochief',
     image: '/images/echoes/Hoochief.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   HoochiefCyclone: {
     key: 'HoochiefCyclone',
     name: 'Hoochief Cyclone',
     image: '/images/echoes/HoochiefCyclone.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   HoochiefMenace: {
     key: 'HoochiefMenace',
     name: 'Hoochief Menace',
     image: '/images/echoes/HoochiefMenace.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   Hooscamp: {
     key: 'Hooscamp',
     name: 'Hooscamp',
     image: '/images/echoes/Hooscamp.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   HooscampClapperclaw: {
     key: 'HooscampClapperclaw',
     name: 'Hooscamp Clapperclaw',
     image: '/images/echoes/HooscampClapperclaw.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   HooscampFlinger: {
     key: 'HooscampFlinger',
     name: 'Hooscamp Flinger',
     image: '/images/echoes/HooscampFlinger.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   ImpermanenceHeron: {
     key: 'ImpermanenceHeron',
     name: 'Impermanence Heron',
     image: '/images/echoes/ImpermanenceHeron.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   InfernoRider: {
     key: 'InfernoRider',
     name: 'Inferno Rider',
     image: '/images/echoes/InfernoRider.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   Jué: {
     key: 'Jué',
     name: 'Jué',
     image: '/images/echoes/Jué.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   LampylumenMyriad: {
     key: 'LampylumenMyriad',
     name: 'Lampylumen Myriad',
     image: '/images/echoes/LampylumenMyriad.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   Lightcrusher: {
     key: 'Lightcrusher',
     name: 'Lightcrusher',
     image: '/images/echoes/Lightcrusher.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   LumiscaleConstruct: {
     key: 'LumiscaleConstruct',
     name: 'Lumiscale Construct',
     image: '/images/echoes/LumiscaleConstruct.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   MechAbomination: {
     key: 'MechAbomination',
     name: 'Mech Abomination',
     image: '/images/echoes/MechAbomination.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   MourningAix: {
     key: 'MourningAix',
     name: 'Mourning Aix',
     image: '/images/echoes/MourningAix.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   PhantomFeilianBeringal: {
     key: 'PhantomFeilianBeringal',
     name: 'Phantom: Feilian Beringal',
     image: '/images/echoes/PhantomFeilianBeringal.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   PhantomHoartoise: {
     key: 'PhantomHoartoise',
     name: 'Phantom: Hoartoise',
     image: '/images/echoes/PhantomHoartoise.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   PhantomImpermanenceHeron: {
     key: 'PhantomImpermanenceHeron',
     name: 'Phantom: Impermanence Heron',
     image: '/images/echoes/PhantomImpermanenceHeron.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   PhantomMourningAix: {
     key: 'PhantomMourningAix',
     name: 'Phantom: Mourning Aix',
     image: '/images/echoes/PhantomMourningAix.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   PhantomRocksteadyGuardian: {
     key: 'PhantomRocksteadyGuardian',
     name: 'Phantom: Rocksteady Guardian',
     image: '/images/echoes/PhantomRocksteadyGuardian.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   PhantomThunderingMephis: {
     key: 'PhantomThunderingMephis',
     name: 'Phantom: Thundering Mephis',
     image: '/images/echoes/PhantomThunderingMephis.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   RocksteadyGuardian: {
     key: 'RocksteadyGuardian',
     name: 'Rocksteady Guardian',
     image: '/images/echoes/RocksteadyGuardian.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   Roseshroom: {
     key: 'Roseshroom',
     name: 'Roseshroom',
     image: '/images/echoes/Roseshroom.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   SabyrBoar: {
     key: 'SabyrBoar',
     name: 'Sabyr Boar',
     image: '/images/echoes/SabyrBoar.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   Scar: {
     key: 'Scar',
     name: 'Scar',
     image: '/images/echoes/Scar.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   SnipSnap: {
     key: 'SnipSnap',
     name: 'Snip Snap',
     image: '/images/echoes/SnipSnap.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   Spearback: {
     key: 'Spearback',
     name: 'Spearback',
     image: '/images/echoes/Spearback.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   SpectroPrism: {
     key: 'SpectroPrism',
     name: 'Spectro Prism',
     image: '/images/echoes/SpectroPrism.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   StonewallBracer: {
     key: 'StonewallBracer',
     name: 'Stonewall Bracer',
     image: '/images/echoes/StonewallBracer.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   TempestMephis: {
     key: 'TempestMephis',
     name: 'Tempest Mephis',
     image: '/images/echoes/TempestMephis.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   ThunderingMephis: {
     key: 'ThunderingMephis',
     name: 'Thundering Mephis',
     image: '/images/echoes/ThunderingMephis.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   TickTack: {
     key: 'TickTack',
     name: 'Tick Tack',
     image: '/images/echoes/TickTack.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   TrafficIlluminator: {
     key: 'TrafficIlluminator',
     name: 'Traffic Illuminator',
     image: '/images/echoes/TrafficIlluminator.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   VanguardJunrock: {
     key: 'VanguardJunrock',
     name: 'Vanguard Junrock',
     image: '/images/echoes/VanguardJunrock.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   VioletFeatheredHeron: {
     key: 'VioletFeatheredHeron',
     name: 'Violet-Feathered Heron',
     image: '/images/echoes/VioletFeatheredHeron.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   WhiffWhaff: {
     key: 'WhiffWhaff',
     name: 'Whiff Whaff',
     image: '/images/echoes/WhiffWhaff.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   YoungGeohideSaurian: {
     key: 'YoungGeohideSaurian',
     name: 'Young Geohide Saurian',
     image: '/images/echoes/YoungGeohideSaurian.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   YoungRoseshroom: {
     key: 'YoungRoseshroom',
     name: 'Young Roseshroom',
     image: '/images/echoes/YoungRoseshroom.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   },
   ZigZag: {
     key: 'ZigZag',
     name: 'Zig Zag',
     image: '/images/echoes/ZigZag.png',
     details: ``,
-    modifiers: {}
+    modifiers: []
   }
 };
 
@@ -539,28 +490,10 @@ export const allEchoes: string[] = [
   "Dwarf Cassowary",
   "Electro Predator",
   "Excarat",
-  "Exile",
-  "Exile",
-  "Exile",
-  "Exile",
-  "Exile",
-  "Exile",
-  "Exile",
-  "Exile",
-  "Exile",
-  "Exile",
-  "Exile",
-  "Exile",
-  "Exile Leader",
-  "Exile Technician",
   "Feilian Beringal",
   "Firebug",
   "Fission Junrock",
   "Flautist",
-  "Fractsidus Cannoneer",
-  "Fractsidus Executioner",
-  "Fractsidus Gunmaster",
-  "Fractsidus Thruster",
   "Fusion Dreadmane",
   "Fusion Prism",
   "Fusion Warrior",
