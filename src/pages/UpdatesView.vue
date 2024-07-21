51d951f1b63507569cc0dddb4fe6c17410433114
Ryan Benson <benson.ryan@gmail.com>
2024-07-21 10:07:16 -0500

    Updates banner and updates page

--- src/pages/UpdatesView.vue
+++ src/pages/UpdatesView.vue
@@ -11,6 +11,10 @@
       <li>More!</li>
     </ul>
     <h1>Updates</h1>
+    <h3>July 21, 2024</h3>
+    <ul>
+      <li>Sanhua is now available.</li>
+    </ul>
     <h3>July 20, 2024</h3>
     <ul>
       <li>
