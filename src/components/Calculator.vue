743c9de818a2f4195518693b25ea80f079dd07d0
Ryan Benson <benson.ryan@gmail.com>
2024-08-15 14:59:19 -0500

    Adds release info

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -37,7 +37,7 @@
     <div class="calculations__screens">
       <div class="screen--character" v-show="curScreen === 'character'">
         <div>
-          <div v-if="false" class="alert">Aalto is now available.</div>
+          <div class="alert">Verina is now available.</div>
           <div class="character__selection">
             <div
               class="character__selection__avatar"
