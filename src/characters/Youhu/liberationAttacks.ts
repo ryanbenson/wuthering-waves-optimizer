2173b5cbbcc0893bbe84c7ca69753e799a0abf20
Ryan Benson <benson.ryan@gmail.com>
2024-08-29 20:52:59 -0500

    Adds almost all of Youhu

---
+++ src/characters/Youhu/liberationAttacks.ts
@@ -0,0 +1,23 @@
+export const liberationAttacks = {
+  name: "Resonance Liberation: Fortune's Favor",
+  description: `<div class="skilldescription"></div>`,
+  attacks: [
+    {
+      key: "FortunesFavorDamage",
+      label: "Skill DMG",
+      talents: {
+        "1": "164.58%",
+        "2": "178.07%",
+        "3": "191.57%",
+        "4": "210.46%",
+        "5": "223.95%",
+        "6": "239.47%",
+        "7": "261.06%",
+        "8": "282.66%",
+        "9": "304.25%",
+        "10": "327.19%",
+      },
+      type: "Liberation",
+    },
+  ],
+};
