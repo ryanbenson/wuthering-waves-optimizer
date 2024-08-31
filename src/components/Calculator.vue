df7bf6fc23a70fe94339fbc7c2683b8f64083d2e
Ryan Benson <benson.ryan@gmail.com>
2024-08-31 13:21:36 -0500

    Updates banner for rotations

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -55,7 +55,7 @@
     <div class="calculations__screens">
       <div class="screen--character" v-show="curScreen === 'character'">
         <div>
-          <div class="alert">Youhu is now available. ❄️</div>
+          <div class="alert">Character rotations are now available. ↻</div>
           <div class="character__selection">
             <div
               class="character__selection__avatar"
