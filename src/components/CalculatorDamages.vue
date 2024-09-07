8c7b88845ee2152d22c51aa15964d4f065520dca
Ryan Benson <benson.ryan@gmail.com>
2024-09-07 10:13:44 -0500

    Adds skill descriptions to be seen

--- src/components/CalculatorDamages.vue
+++ src/components/CalculatorDamages.vue
@@ -10,7 +10,19 @@
     <span>Average</span>
     <span>Crit</span>
   </div>
-  <h4>{{ chosenChar.value?.basicAttacks?.name ?? "Basic Attacks" }}</h4>
+  <h4 class="damage__title">
+    <span>{{ chosenChar.value?.basicAttacks?.name ?? "Basic Attacks" }}</span>
+    <span class="damage__title__button" @click="toggleBasicDetails"
+      ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+        <path
+          d="M64 464c-8.8 0-16-7.2-16-16L48 64c0-8.8 7.2-16 16-16l160 0 0 80c0 17.7 14.3 32 32 32l80 0 0 288c0 8.8-7.2 16-16 16L64 464zM64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-293.5c0-17-6.7-33.3-18.7-45.3L274.7 18.7C262.7 6.7 246.5 0 229.5 0L64 0zm56 256c-13.3 0-24 10.7-24 24s10.7 24 24 24l144 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-144 0zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24l144 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-144 0z"
+          fill="#FFFFFF" /></svg
+    ></span>
+  </h4>
+  <div
+    v-if="isBasicDetailsShown"
+    v-html="chosenChar.value?.basicAttacks?.description"
+    class="panel mb-1"></div>
   <CalculatorDamage
     v-for="damageInstance in allDamages?.value?.basicAttacks"
     :key="damageInstance.key"
@@ -19,7 +31,19 @@
     :label="damageInstance.label"
     :damage="damageInstance.damage">
   </CalculatorDamage>
-  <h4>{{ chosenChar.value?.skillAttacks?.name ?? "Skill Attacks" }}</h4>
+  <h4 class="damage__title">
+    <span>{{ chosenChar.value?.skillAttacks?.name ?? "Skill Attacks" }}</span>
+    <span class="damage__title__button" @click="toggleSkillDetails"
+      ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+        <path
+          d="M64 464c-8.8 0-16-7.2-16-16L48 64c0-8.8 7.2-16 16-16l160 0 0 80c0 17.7 14.3 32 32 32l80 0 0 288c0 8.8-7.2 16-16 16L64 464zM64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-293.5c0-17-6.7-33.3-18.7-45.3L274.7 18.7C262.7 6.7 246.5 0 229.5 0L64 0zm56 256c-13.3 0-24 10.7-24 24s10.7 24 24 24l144 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-144 0zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24l144 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-144 0z"
+          fill="#FFFFFF" /></svg
+    ></span>
+  </h4>
+  <div
+    v-if="isSkillDetailsShown"
+    v-html="chosenChar.value?.skillAttacks?.description"
+    class="panel mb-1"></div>
   <CalculatorDamage
     v-for="damageInstance in allDamages?.value?.skillAttacks"
     :key="damageInstance.key"
@@ -28,9 +52,21 @@
     :label="damageInstance.label"
     :damage="damageInstance.damage">
   </CalculatorDamage>
-  <h4>
-    {{ chosenChar.value?.liberationAttacks?.name ?? "Liberation Attacks" }}
+  <h4 class="damage__title">
+    <span>{{
+      chosenChar.value?.liberationAttacks?.name ?? "Liberation Attacks"
+    }}</span>
+    <span class="damage__title__button" @click="toggleLiberationDetails"
+      ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+        <path
+          d="M64 464c-8.8 0-16-7.2-16-16L48 64c0-8.8 7.2-16 16-16l160 0 0 80c0 17.7 14.3 32 32 32l80 0 0 288c0 8.8-7.2 16-16 16L64 464zM64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-293.5c0-17-6.7-33.3-18.7-45.3L274.7 18.7C262.7 6.7 246.5 0 229.5 0L64 0zm56 256c-13.3 0-24 10.7-24 24s10.7 24 24 24l144 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-144 0zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24l144 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-144 0z"
+          fill="#FFFFFF" /></svg
+    ></span>
   </h4>
+  <div
+    v-if="isLiberationDetailsShown"
+    v-html="chosenChar.value?.liberationAttacks?.description"
+    class="panel mb-1"></div>
   <CalculatorDamage
     v-for="damageInstance in allDamages?.value?.liberationAttacks"
     :key="damageInstance.key"
@@ -39,9 +75,21 @@
     :label="damageInstance.label"
     :damage="damageInstance.damage">
   </CalculatorDamage>
-  <h4>
-    {{ chosenChar.value?.forteCircuitAttacks?.name ?? "Forte Circuit Attacks" }}
+  <h4 class="damage__title">
+    <span>{{
+      chosenChar.value?.forteCircuitAttacks?.name ?? "Forte Circuit Attacks"
+    }}</span>
+    <span class="damage__title__button" @click="toggleForteCircuitDetails"
+      ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+        <path
+          d="M64 464c-8.8 0-16-7.2-16-16L48 64c0-8.8 7.2-16 16-16l160 0 0 80c0 17.7 14.3 32 32 32l80 0 0 288c0 8.8-7.2 16-16 16L64 464zM64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-293.5c0-17-6.7-33.3-18.7-45.3L274.7 18.7C262.7 6.7 246.5 0 229.5 0L64 0zm56 256c-13.3 0-24 10.7-24 24s10.7 24 24 24l144 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-144 0zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24l144 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-144 0z"
+          fill="#FFFFFF" /></svg
+    ></span>
   </h4>
+  <div
+    v-if="isForteCircuitDetailsShown"
+    v-html="chosenChar.value?.forteCircuitAttacks?.description"
+    class="panel mb-1"></div>
   <CalculatorDamage
     v-for="damageInstance in allDamages?.value?.forteCircuitAttacks"
     :key="damageInstance.key"
@@ -50,7 +98,19 @@
     :label="damageInstance.label"
     :damage="damageInstance.damage">
   </CalculatorDamage>
-  <h4>{{ chosenChar.value?.introAttacks?.name ?? "Intro Attacks" }}</h4>
+  <h4 class="damage__title">
+    <span>{{ chosenChar.value?.introAttacks?.name ?? "Intro Attacks" }}</span>
+    <span class="damage__title__button" @click="toggleIntroDetails"
+      ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+        <path
+          d="M64 464c-8.8 0-16-7.2-16-16L48 64c0-8.8 7.2-16 16-16l160 0 0 80c0 17.7 14.3 32 32 32l80 0 0 288c0 8.8-7.2 16-16 16L64 464zM64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-293.5c0-17-6.7-33.3-18.7-45.3L274.7 18.7C262.7 6.7 246.5 0 229.5 0L64 0zm56 256c-13.3 0-24 10.7-24 24s10.7 24 24 24l144 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-144 0zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24l144 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-144 0z"
+          fill="#FFFFFF" /></svg
+    ></span>
+  </h4>
+  <div
+    v-if="isIntroDetailsShown"
+    v-html="chosenChar.value?.introAttacks?.description"
+    class="panel mb-1"></div>
   <CalculatorDamage
     v-for="damageInstance in allDamages?.value?.introAttacks"
     :key="damageInstance.key"
@@ -59,7 +119,19 @@
     :label="damageInstance.label"
     :damage="damageInstance.damage">
   </CalculatorDamage>
-  <h4>{{ chosenChar.value?.outroAttacks?.name ?? "Outro Attacks" }}</h4>
+  <h4 class="damage__title">
+    <span>{{ chosenChar.value?.outroAttacks?.name ?? "Outro Attacks" }}</span>
+    <span class="damage__title__button" @click="toggleOutroDetails"
+      ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+        <path
+          d="M64 464c-8.8 0-16-7.2-16-16L48 64c0-8.8 7.2-16 16-16l160 0 0 80c0 17.7 14.3 32 32 32l80 0 0 288c0 8.8-7.2 16-16 16L64 464zM64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-293.5c0-17-6.7-33.3-18.7-45.3L274.7 18.7C262.7 6.7 246.5 0 229.5 0L64 0zm56 256c-13.3 0-24 10.7-24 24s10.7 24 24 24l144 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-144 0zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24l144 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-144 0z"
+          fill="#FFFFFF" /></svg
+    ></span>
+  </h4>
+  <div
+    v-if="isOutroDetailsShown"
+    v-html="chosenChar.value?.outroAttacks?.description"
+    class="panel mb-1"></div>
   <template v-if="!allDamages?.value?.outroAttacks.length">
     <div class="calculation__damage__item calculation__damage__item--fill">
       {{ character }} does not have outro attacks.
@@ -124,8 +196,36 @@ export default {
   components: {
     CalculatorDamage,
   },
+  data() {
+    return {
+      isBasicDetailsShown: false,
+      isSkillDetailsShown: false,
+      isLiberationDetailsShown: false,
+      isForteCircuitDetailsShown: false,
+      isIntroDetailsShown: false,
+      isOutroDetailsShown: false,
+    };
+  },
   methods: {
     displayDamage,
+    toggleBasicDetails() {
+      this.isBasicDetailsShown = !this.isBasicDetailsShown;
+    },
+    toggleSkillDetails() {
+      this.isSkillDetailsShown = !this.isSkillDetailsShown;
+    },
+    toggleLiberationDetails() {
+      this.isLiberationDetailsShown = !this.isLiberationDetailsShown;
+    },
+    toggleForteCircuitDetails() {
+      this.isForteCircuitDetailsShown = !this.isForteCircuitDetailsShown;
+    },
+    toggleIntroDetails() {
+      this.isIntroDetailsShown = !this.isIntroDetailsShown;
+    },
+    toggleOutroDetails() {
+      this.isOutroDetailsShown = !this.isOutroDetailsShown;
+    },
   },
 };
 </script>
@@ -190,5 +290,34 @@ export default {
 }
 .mb-1 {
   margin-bottom: 1rem;
+}
+.damage__title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .damage__title__button {
+    background-color: rgba(0, 0, 0, 0.3);
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 36px;
+    height: 36px;
+    cursor: pointer;
+  }
+
+  svg {
+    width: 1rem;
+    height: 1rem;
+  }
+}
+</style>
+
+<style lang="scss">
+.skilldescription {
+  .Title {
+    font-weight: bold;
+  }
 }
 </style>
