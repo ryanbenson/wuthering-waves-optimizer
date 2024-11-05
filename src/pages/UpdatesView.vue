cd6c93980c2af21e85206eca1039a3dccdb2f464
Ryan Benson <benson.ryan@gmail.com>
2024-11-04 19:26:04 -0600

    Adds 30 for flat atk and def

--- src/pages/UpdatesView.vue
+++ src/pages/UpdatesView.vue
@@ -99,6 +99,7 @@
       <li>Updates Camellya signature for 1.4.7 beta updates.</li>
       <li>UI Update: Adjusts the hidden text to be more legible.</li>
       <li>Fixes flat HP value, had a typo.</li>
+      <li>Adds missing flat ATK and DEF (30).</li>
     </ul>
     <h3>November 3, 2024</h3>
     <ul>
