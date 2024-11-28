5c817bb2604fec09227efd68c7030505db15dc4b
Ryan Benson <benson.ryan@gmail.com>
2024-11-28 11:56:04 -0600

    Fixes Calcharo sequence titles

--- src/characters/Calcharo/resonanceChains.ts
+++ src/characters/Calcharo/resonanceChains.ts
@@ -30,8 +30,8 @@ export const resonanceChains = [
     alwaysEnabled: false,
   },
   {
-    key: "Sequence Node 4: Dark Alliance",
-    name: "SequenceNode4DarkAlliance",
+    key: "SequenceNode4DarkAlliance",
+    name: "Sequence Node 4: Dark Alliance",
     details: `<span class="skilldescription">After casting Outro Skill <span class="Highlight">Shadowy Raid</span>, Electro DMG Bonus of all team members is increased by 20% for 30s.</span>`,
     hasStacks: false,
     modifiers: [
@@ -45,8 +45,8 @@ export const resonanceChains = [
     alwaysEnabled: false,
   },
   {
-    key: "Sequence Node 5: Unconventional Compact",
-    name: "SequenceNode5UnconventionalCompact",
+    key: "SequenceNode5UnconventionalCompact",
+    name: "Sequence Node 5: Unconventional Compact",
     details: `<span class="skilldescription">Intro Skill <span class="Highlight">Wanted Criminal</span> and Intro Skill <span class="Highlight">"Necessary Means:</span> deal 50% more damage.</span>`,
     hasStacks: false,
     modifiers: [
@@ -60,8 +60,8 @@ export const resonanceChains = [
     alwaysEnabled: false,
   },
   {
-    key: "Sequence Node 6: The Ultimatum",
-    name: "SequenceNode6TheUltimatum",
+    key: "SequenceNode6TheUltimatum",
+    name: "Sequence Node 6: The Ultimatum",
     details: `<span class="skilldescription">When casting Resonance Liberation <span class="Highlight">"Death Messenger"</span>, Calcharo will summon 2 <span class="Highlight">Phantoms</span> to perform Coordinated Attacks. Each <span class="Highlight">Phantom</span> deals <span class="Thunder">Electro DMG</span> equal to 100.00% of Calcharo's ATK, which is considered Resonance Liberation DMG.</span>`,
     hasStacks: false,
     modifiers: [
