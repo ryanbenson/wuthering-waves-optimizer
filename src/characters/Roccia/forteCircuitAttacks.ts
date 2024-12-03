e42748d37496194e92c108d711bed5be3043af1d
Ryan Benson <benson.ryan@gmail.com>
2024-12-03 08:56:14 -0600

    Fixes title for Roccia forte

--- src/characters/Roccia/forteCircuitAttacks.ts
+++ src/characters/Roccia/forteCircuitAttacks.ts
@@ -9,7 +9,7 @@ This state will end when Roccia lands.<br>
 Performs up to 3 consecutive attacks, dealing <span class="Dark">Havoc damage</span>. This damage is considered <span class="Highlight">Resonance Skill</span> damage. After the 1st and 2nd attacks land, Roccia will be launched into the air, entering the Leap Fantasy state.<br>
 When Roccia's critical rate is above <span class="Highlight">50%</span>, for every additional <span class="Highlight">0.1% critical rate</span>, the 3rd attack of the Basic Attack Fantasy into reality will increase the attack of all characters in the team by <span class="Highlight">1</span> point, lasting for <span class="Highlight">30</span> seconds. This can be increased up to <span class="Highlight">200</span> points.<br>
 <br>
-<span class="Title">【Imagination】 Acquisition Rules<br>
+<span class="Title">【Imagination】 Acquisition Rules</span><br>
 The maximum 【Imagination】 is 300 points.<br>
 Normal Attack damage restores 【Imagination】.<br>
 Casting the Resonance Skill High-Difficulty Design or the Intro Skill Perlo, Come Help restores 【Imagination】.
