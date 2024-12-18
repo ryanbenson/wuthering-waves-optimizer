a4109878103da40af8d03b6ce917d8e993f8ac8c
Ryan Benson <benson.ryan@gmail.com>
2024-12-17 21:55:04 -0600

    Fixes Yuanwu DEF stat bonuses

--- src/pages/UpdatesView.vue
+++ src/pages/UpdatesView.vue
@@ -104,6 +104,10 @@
     <h3>December 17, 2024</h3>
     <ul>
       <li>Fixes spelling error on notice for enemy settings.</li>
+      <li>
+        Fixes Yuanwu's 1st set of DEF stat bonuses. The percent was slightly
+        off.
+      </li>
     </ul>
     <h3>December 6, 2024</h3>
     <ul>
