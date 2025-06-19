fa2b3d08df4f898753d1f686bef544acfdc386e4
Ryan Benson <benson.ryan@gmail.com>
2025-06-18 21:16:58 -0500

    Base slideout

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,4 +1,59 @@
 <template>
+  <div class="drawer drawer-end z-50">
+    <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+      <!-- Page content here -->
+      <label for="my-drawer-4" class="drawer-button btn btn-primary">
+        Open drawer
+      </label>
+    </div>
+    <div class="drawer-side">
+      <label
+        for="my-drawer-4"
+        aria-label="close sidebar"
+        class="drawer-overlay"></label>
+      <div class="bg-base-200 text-base-content min-h-full w-80 p-4">
+        <!-- Sidebar content here -->
+        <h3 class="text-2xl font-bold mb-4">HP</h3>
+        <hr />
+        <p>
+          Base:
+          <strong>14800</strong>
+        </p>
+        <p>HP%:</p>
+        <ul class="mt-2">
+          <li>
+            Stat Bonuses:
+            <strong>12%</strong>
+          </li>
+          <li>
+            Echoes:
+            <strong>180%</strong>
+          </li>
+          <li>
+            Custom Buffs:
+            <strong>20%</strong>
+          </li>
+        </ul>
+        <p>Flat HP:</p>
+        <ul class="mt-2">
+          <li>
+            Echoes:
+            <strong>7800</strong>
+          </li>
+          <li>
+            Custom Buffs:
+            <strong>500</strong>
+          </li>
+        </ul>
+        <hr />
+        <p class="mt-4">
+          Total:
+          <strong>46000</strong>
+        </p>
+      </div>
+    </div>
+  </div>
   <div class="calculator__content">
     <div class="calculator__el">
       <Calculator class="calculator" :key="key"></Calculator>
