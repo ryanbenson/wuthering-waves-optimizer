d18e9116ac562ec6141d87c973b4a1619aaff7f4
Ryan Benson <benson.ryan@gmail.com>
2025-07-20 12:30:04 -0500

    WIP Phrolova

---
+++ src/characters/Phrolova/basic.ts
@@ -0,0 +1,10 @@
+export function getCharacterBasicInfo(): CharacterBasicInfo {
+  return {
+    name: "Phrolova",
+    rarity: 5,
+    weapon: "Rectifiers",
+    avatarUrl: "Phrolova.png",
+    gender: "female",
+    element: "Havoc",
+  };
+}
