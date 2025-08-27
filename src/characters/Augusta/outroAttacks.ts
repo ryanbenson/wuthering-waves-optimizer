5f4074a36c5e872d872cdc95f5cbae0af38472b3
Ryan Benson <benson.ryan@gmail.com>
2025-08-26 20:49:00 -0500

    Almost done with Augusta

--- src/characters/Augusta/outroAttacks.ts
+++ src/characters/Augusta/outroAttacks.ts
@@ -1,5 +1,5 @@
 export const outroAttacks = {
-  name: "Outro Skill: Stand by Me, Warrior",
-  description: `The incoming Resonator will have their Fusion DMG Amplified by 20% and Basic Attack DMG Amplified by 25% for 14s or until the Resonator is switched out.`,
+  name: "Outro Skill: Battlesong of the Unyielding",
+  description: `<div>The next Resonator switched onto the field gains the following effects for 14s, which end immediately if they are switched out:<br>- Gain 15% DMG Amplification for all Attributes.<br>- Casting Outro Skill grants 1 stack of <a onclick="showTermExplan(130604)" class="ingame-term-desc">Majesty</a> and 1 stack of <a onclick="showTermExplan(130601)" class="ingame-term-desc">Crown of Wills</a> to Augusta.<br><span class="ww-wiki-font-bold"></span><br><span class="ww-wiki-font-bold"><span class="ingame-Title">Crown of Wills</span></span><br>Each stack grants 15% Electro DMG Bonus, up to 1 stack. When <span class="ingame-Highlight">Resonance Liberation - Sublime is the Sun: Everbright Protector</span> ends, all stacks of Crown of Wills are removed.</div>`,
   attacks: [],
 };
--- src/characters/Augusta/resonanceChains.ts
+++ src/characters/Augusta/resonanceChains.ts
@@ -1,28 +1,28 @@
 export const resonanceChains = [
   {
-    key: "SequenceNode1BeholdtheNamelessOne",
-    name: "Sequence Node 1: Behold the Nameless One",
-    details: `<span class="skilldescription">Performing Resonance Skill <span class="Highlight">Fire-Kissed Glory</span> recovers 10 Concerto Energy for Lupa and increases Lupa's Crit. Rate by 20% for 10s.<br><span class="Highlight">Dance With the Wolf - Climax</span> is immune to interruption.</span>`,
-    hasStacks: false,
-    modifiers: [
-      {
-        modifier: "CritRate",
-        modifierValue: 0.2,
-      },
-    ],
-    minStacks: 0,
-    maxStacks: 0,
-    alwaysEnabled: false,
-  },
-  {
-    key: "SequenceNode2EveryGroundHerHuntingField",
-    name: "Sequence Node 2: Every Ground, Her Hunting Field",
-    details: `<span class="skilldescription">Performing <span class="Highlight">Fire-Kissed Glory</span>, <span class="Highlight">Heavy Attack - Wolf's Gnawing</span>,  <span class="Highlight">Heavy Attack - Wolf's Claw</span>, or <span class="Highlight">Mid-air Attack - Firestrike</span> gives 20% Fusion DMG Bonus to all Resonators in the team for 30s, stacking up to 2 times.</span>`,
+    key: "SequenceNode1StainedinScorchedEarthExtraElectroBuff",
+    name: "Sequence Node 1: Stained in Scorched Earth - Extra Electro Buff",
+    details: `<div>The max stack of Crown of Wills is increased to 2.<br>- Casting <span class="ingame-Highlight">Intro Skill - Stride of Goldenflare</span> now grants 1 stack of Crown of Wills. <br>- <span class="ingame-Highlight">Resonance Skill - Undying Sunlight: Strike</span>, <span class="ingame-Highlight">Resonance Skill - Undying Sunlight: Leap</span>, and <span class="ingame-Highlight">Resonance Skill - Undying Sunlight: Plunge</span> are now immune to interruption.</div>`,
     hasStacks: true,
     modifiers: [
       {
-        modifier: "Fusion",
-        modifierValue: 0.2,
+        modifier: "Electro",
+        modifierValue: 0.15,
+      },
+    ],
+    minStacks: 0,
+    maxStacks: 1,
+    alwaysEnabled: false,
+  },
+  {
+    key: "SequenceNode1StainedinScorchedEarthCritDMG",
+    name: "Sequence Node 1: Stained in Scorched Earth - Crit DMG",
+    details: `<div>- Each stack of Crown of Wills additionally increases Augusta's Crit. DMG by 15%. <br>- The max stack of Crown of Wills is increased to 2.</div>`,
+    hasStacks: true,
+    modifiers: [
+      {
+        modifier: "CritDMG",
+        modifierValue: 0.15,
       },
     ],
     minStacks: 0,
@@ -30,99 +30,120 @@ export const resonanceChains = [
     alwaysEnabled: false,
   },
   {
-    key: "SequenceNode3WolflameHowlsinHerWake",
-    name: "Sequence Node 3: Wolflame Howls in Her Wake",
-    details: `<span class="skilldescription">The DMG Multiplier of Intro Skill <span class="Highlight">Nowhere to Run!</span> increases by 100%.</span>`,
+    key: "SequenceNode2CleansedinCrimsonWar",
+    name: "Sequence Node 2: Cleansed in Crimson War",
+    details: `<div>- <a onclick="showTermExplan(130601)" class="ingame-term-desc">Crown of Wills</a> provides additional effects: Each stack increases Augusta's Crit. Rate by 20%.<br>- For every 1% of Crit. Rate over 100%, Augusta gains 2% Crit. DMG increase, up to 100%.</div>`,
+    hasStacks: true,
+    modifiers: [
+      {
+        modifier: "CritRate",
+        modifierValue: 0.2,
+      },
+      {
+        modifier: "CritOverflow",
+        modifierValue: 0.02,
+        overflowStep: 1, // for every 1% CR
+        overflowMin: 1, // must be 100% CR
+        overflowMax: 1, // can only get 100% CD from this
+      },
+    ],
+    minStacks: 0,
+    maxStacks: 2,
+    alwaysEnabled: false,
+  },
+  {
+    key: "SequenceNode3ForgedinRotandRuin",
+    name: "Sequence Node 3: Forged in Rot and Ruin",
+    details: `<div>The following skills have their DMG Multiplier increased by 25%: <br>- <span class="ingame-Highlight">Heavy Attack - Thunderoar: Backstep</span>, <span class="ingame-Highlight">Dodge Counter - Thunderoar: Backstep</span>, <span class="ingame-Highlight">Heavy Attack - Thunderoar: Spinslash</span>, <span class="ingame-Highlight">Heavy Attack - Thunderoar: Uppercut</span>.<br>- <span class="ingame-Highlight">Resonance Skill - Undying Sunlight: Plunge</span>.<br>- <span class="ingame-Highlight">Resonance Liberation - Sublime is the Sun: Sunborne</span>, <span class="ingame-Highlight">Resonance Liberation - Sublime is the Sun: Everbright Protector</span>.</div>`,
     hasStacks: false,
     modifiers: [
       {
         modifier: "talentModifierMultiply",
-        modifySpecificTalents: ["NowheretoRunDMG"],
-        modifierValue: 1,
-      },
-    ],
-    minStacks: 0,
-    maxStacks: 0,
-    alwaysEnabled: false,
-  },
-  {
-    key: "SequenceNode3WolflameHowlsinHerWakeIgnoreFusion",
-    name: "Sequence Node 3: Wolflame Howls in Her Wake: Fusion RES Ignore",
-    details: `<span class="skilldescription">The effect of Resonance Liberation <span class="Highlight">Glory</span> is now updated as follows:<br>Casting Resonance Liberation <span class="Highlight">Fire-Kissed Glory</span> grants <span class="Highlight">Glory</span>. Attacks of all Resonators in the team ignore 15% Fusion RES for 35s.</span>`,
-    hasStacks: false,
-    modifiers: [
-      {
-        modifier: "ResistShred:Fusion",
-        modifierValue: 0.15,
-      },
-    ],
-    minStacks: 0,
-    maxStacks: 0,
-    alwaysEnabled: false,
-    replaces: "InherentSkillApplauseofVictory",
-  },
-  {
-    key: "SequenceNode4HighandAflameIsHerBanner",
-    name: "Sequence Node 4: High and Aflame Is Her Banner",
-    details: `<span class="skilldescription">The DMG Multiplier of <span class="Highlight">Dance With the Wolf - Climax</span> increases by 125%.</span>`,
-    hasStacks: false,
-    modifiers: [
-      {
-        modifier: "talentModifierMultiply",
-        modifySpecificTalents: ["DanceWiththeWolfClimaxDMG"],
-        modifierValue: 1.25,
-      },
-    ],
-    minStacks: 0,
-    maxStacks: 0,
-    alwaysEnabled: false,
-  },
-  {
-    key: "SequenceNode5EmbracetheThunderousTriumph",
-    name: "Sequence Node 5: Embrace the Thunderous Triumph",
-    details: `<span class="skilldescription">Performing Intro Skill <span class="Highlight">Try Focusing, Eh?</span> or <span class="Highlight">Nowhere to Run!</span> gives 15% Resonance Liberation DMG Bonus for 10s.</span>`,
-    hasStacks: false,
-    modifiers: [
-      {
-        modifier: "ResonanceLiberationDMGBonus",
-        modifierValue: 0.15,
-      },
-    ],
-    minStacks: 0,
-    maxStacks: 0,
-    alwaysEnabled: false,
-  },
-  {
-    key: "SequenceNode6TotheBrightestFlamingStar",
-    name: "Sequence Node 6: To the Brightest Flaming Star",
-    details: `<span class="skilldescription">- The damage dealt by Forte Circuit <span class="Highlight">Dance With the Wolf - Climax</span>, Resonance Liberation <span class="Highlight">Fire-Kissed Glory</span>, and Intro Skill <span class="Highlight">Nowhere to Run!</span> ignores 30% of the target's DEF.<br>- Resonance Skill <span class="Highlight">Feral Fang</span> restores 100 points of Wolflame on hit, triggered once per 20s.<br>- Forte Circuit <span class="Highlight">Dance With the Wolf</span> is replaced with <span class="Highlight">Dance With the Wolf - Climax</span>. <span class="Highlight">Dance With the Wolf - Climax</span> can be performed when Lupa is not in <span class="Highlight">Burning Matchpoint</span> state.<br>- Casting Intro Skill <span class="Highlight">Nowhere to Run!</span> no longer ends <span class="Highlight">Pack Hunt</span> and <span class="Highlight">Glory</span>.</span>`,
-    hasStacks: false,
-    modifiers: [
-      {
-        modifier: "DEFIgnore",
         modifySpecificTalents: [
-          "DanceWiththeWolfClimaxDMG",
-          "FireKissedGloryDMG",
-          "NowheretoRunDMG",
+          "HeavyAttackThunderoarBackstepDMG",
+          "DodgeCounterThunderoarBackstepDMG",
+          "HeavyAttackThunderoarSpinslashDMG",
+          "HeavyAttackThunderoarUppercutDMG",
+          "ResonanceSkillUndyingSunlightPlungeDMG",
+          "SublimeistheSunSunborneDMG",
+          "SublimeistheSunEverbrightProtectorDMG",
         ],
-        modifierValue: 0.3,
-      },
-      {
-        // TODO: This doesn't work. but it will need to. Essentially, the modifierValue
-        // list is a list of team buffs with a list of attacks it buffs.
-        // we need to add the NowheretoRunDMG to that list when this gets activated.
-        modifier: "AppendAnotherTalent",
-        modifySpecificTalents: ["NowheretoRunDMG"],
-        modifierValue: [
-          "PackHuntFusionTeam",
-          "PackHuntFusionEnhanced",
-          "InherentSkillApplauseofVictory",
-          "InherentSkillApplauseofVictoryFullFusionTeam",
-        ],
+        modifierValue: 0.25,
       },
     ],
     minStacks: 0,
     maxStacks: 0,
+    alwaysEnabled: false,
+  },
+  {
+    key: "SequenceNode4AscentinSunandGlory",
+    name: "Sequence Node 4: Ascent in Sun and Glory",
+    details: `<div>Casting <span class="ingame-Highlight">Intro Skill - Stride of Goldenflare</span> increases the ATK of all Resonators in the team by 20% for 30s.</div>`,
+    hasStacks: false,
+    modifiers: [
+      {
+        modifier: "ATK",
+        modifierValue: 0.2,
+      },
+    ],
+    minStacks: 0,
+    maxStacks: 0,
+    alwaysEnabled: false,
+  },
+  {
+    key: "SequenceNode5UnshakeninWrathfulTides",
+    name: "Sequence Node 5: Unshaken in Wrathful Tides",
+    details: `<div>The Shield provided by <span class="ingame-Highlight">Inherent Skill - Glory's Favor</span> is increased by 50%.</div>`,
+    hasStacks: false,
+    modifiers: [
+      {
+        modifySpecificTalents: ["InherentSkillGlorysFavor"],
+        modifierValue: 0.5,
+      },
+    ],
+    minStacks: 0,
+    maxStacks: 0,
+    alwaysEnabled: false,
+  },
+  {
+    key: "SequenceNode6EngravedinRadiantLight",
+    name: "Sequence Node 6: Engraved in Radiant Light",
+    details: `<div>- For every 1% of Crit. Rate over 150%, Augusta gains 2% Crit. DMG increase, up to 50%.<br>- When Augusta performs <span class="ingame-Highlight">Heavy Attack - Thunderoar: Spinslash</span> or <span class="ingame-Highlight">Heavy Attack - Thunderoar: Uppercut</span>, she obtains 2 stacks of Crown of Wills. Augusta can only obtain 2 stacks of Crown of Wills every 1s via <span class="ingame-Highlight">Engraved in Radiant Light</span>. <br>- While casting <span class="ingame-Highlight">Heavy Attack - Thunderoar: Spinslash</span> or <span class="ingame-Highlight">Heavy Attack - Thunderoar: Uppercut</span>, Thunder Rage is triggered at the spot, dealing two instances of <span class="ingame-Thunder">Electro DMG</span>, with each instance equal to 100% of Augusta's ATK, considered as Heavy Attack DMG.</div>`,
+    hasStacks: false,
+    modifiers: [
+      {
+        modifier: "CritOverflow",
+        modifierValue: 0.02,
+        overflowStep: 1, // for every 1% CR
+        overflowMin: 1.5, // must be 100% CR
+        overflowMax: 0.5, // can only get 100% CD from this
+      },
+      {
+        modifier: "EnableAttack",
+        modifierValue: ["SequenceNode6EngravedinRadiantLight"],
+      },
+    ],
+    minStacks: 0,
+    maxStacks: 0,
+    alwaysEnabled: false,
+  },
+  {
+    key: "SequenceNode6EngravedinRadiantLightCrownofWills",
+    name: "Sequence Node 6: Engraved in Radiant Light - Additional Crown of Wills",
+    details: `<div>- Augusta can now hold up to 4 stacks of Crown of Wills.</div>`,
+    hasStacks: true,
+    modifiers: [
+      {
+        modifier: "CritDMG",
+        modifierValue: 0.15,
+      },
+      {
+        modifier: "Electro",
+        modifierValue: 0.15,
+      },
+    ],
+    minStacks: 0,
+    maxStacks: 2,
     alwaysEnabled: false,
   },
 ];
