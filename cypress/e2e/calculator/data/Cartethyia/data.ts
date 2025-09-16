7d2ad5ebbf045ec5a29e226a871b29ae6648afcc
Ryan Benson <benson.ryan@gmail.com>
2025-09-15 20:37:31 -0500

    Adds more tests

---
+++ cypress/e2e/calculator/data/Cartethyia/data.ts
@@ -0,0 +1,222 @@
+export const config = {
+  meta: { version: "2", source: "WutheringTools" },
+  data: {
+    character:
+      '{"characters":{"Augusta":{"echoes":{"0":{},"1":{},"2":{},"3":{},"4":{}},"mainEcho":{"echo":null,"rank":5}},"Phrolova":{"echoes":{"0":{"type":4,"echo":"NightmareHecate","echoSet":"DreamoftheLost","echoSubStatsType1":"CritRate","echoSubStatsValue1":8.1,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":16.2,"echoSubStatsType3":"ATK","echoSubStatsValue3":8.6,"echoSubStatsType4":"ATK_FLAT","echoSubStatsValue4":50,"echoSubStatsType5":"ResonanceSkillDMGBonus","echoSubStatsValue5":8.6,"stat":"CritDMG"},"1":{"type":3,"echo":"NightmareTambourinist","echoSet":"DreamoftheLost","stat":"Havoc","echoSubStatsType1":"CritRate","echoSubStatsValue1":8.1,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":16.2,"echoSubStatsType3":"ATK","echoSubStatsValue3":8.6,"echoSubStatsType4":"ATK_FLAT","echoSubStatsValue4":50,"echoSubStatsType5":"ResonanceSkillDMGBonus","echoSubStatsValue5":8.6},"2":{"type":3,"echo":"Tambourinist","echoSet":"SunSinkingEclipse","stat":"Havoc","echoSubStatsType1":"CritRate","echoSubStatsValue1":8.1,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":16.2,"echoSubStatsType3":"EnergyRegen","echoSubStatsValue3":7.6,"echoSubStatsType4":"DEF","echoSubStatsValue4":10.9,"echoSubStatsType5":"HP","echoSubStatsValue5":8.6},"3":{"type":1,"echo":"NightmareHavocWarrior","echoSet":"DreamoftheLost","stat":"ATK","echoSubStatsType1":"CritRate","echoSubStatsValue1":8.1,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":16.2,"echoSubStatsType3":"EnergyRegen","echoSubStatsValue3":7.6,"echoSubStatsType4":"HP","echoSubStatsValue4":8.6,"echoSubStatsType5":"DEF","echoSubStatsValue5":10.9},"4":{"type":1,"echo":"HavocWarrior","echoSet":"SunSinkingEclipse","stat":"ATK","echoSubStatsType1":"CritRate","echoSubStatsValue1":8.1,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":16.2,"echoSubStatsType3":"HP_FLAT","echoSubStatsValue3":430,"echoSubStatsType4":"DEF","echoSubStatsValue4":10.9,"echoSubStatsType5":"DEF_FLAT","echoSubStatsValue5":60}},"mainEcho":{"echo":"NightmareHecate","rank":5,"isEnabled":true},"buffs":{"Aftersound":{"isEnabled":true,"stacks":24},"Maestro":{"isEnabled":true},"InherentSkillAccidental":{"isEnabled":true},"InherentSkillOctet":{"isEnabled":true,"stacks":24},"InherentSkillOctetAdditionalStacks":{"isEnabled":true,"stacks":100},"StatBonusCritRate1":{"isEnabled":true},"StatBonusCritRate2":{"isEnabled":true},"StatBonusCritRate4":{"isEnabled":true},"StatBonusCritRate3":{"isEnabled":true},"StatBonusATK1":{"isEnabled":true},"StatBonusATK2":{"isEnabled":true},"StatBonusATK3":{"isEnabled":true},"StatBonusATK4":{"isEnabled":true}},"weapon":"LetheanElegy","weaponPassives":{"LetheanElegyATK":{"isEnabled":true},"LetheanElegySkillDMGBonus":{"isEnabled":true},"LetheanElegyEchoDMGAmplification":{"isEnabled":true},"LetheanElegyDefIgnore":{"isEnabled":true}},"echoSetBonus":{"setBonusOne":"Sun-sinking Eclipse 2 Set","setBonusTwo":"Dream of the Lost 3 Set"},"echoSetPassives":{"SunSinkingEclipse2SetHavoc":{"isEnabled":true},"DreamoftheLostCritRateEchoSkill":{"isEnabled":true}},"resonanceChains":{"SequenceNode1AKeytoNetherworldsSecrets":{"isEnabled":true},"SequenceNode2ARopeTiedtoaLifeBeyond":{"isEnabled":true},"SequenceNode3ADaggertoCutCleanObsessions":{"isEnabled":true},"SequenceNode5AForkedRoadinFatesHeartland":{"isEnabled":true},"SequenceNode6ANighttoDepartFromEternalRest":{"isEnabled":true},"SequenceNode6ANighttoDepartFromEternalRestOffField":{"isEnabled":false},"SequenceNode6ANighttoDepartFromEternalRestOnField":{"isEnabled":true}},"teamBuffs":{"selectedCharacter1":"Roccia","selectedCharacter2":"Cantarella","buffs":{"InherentSkillSuperAttractiveMagicBox":{"isEnabled":true},"CommediaImprovvisoATK":{"isEnabled":true,"baseAttrValue":100},"OutroSkillApplausePlease!":{"isEnabled":true},"SequenceNode2WhentheLuceaniteGleamsHavoc":{"isEnabled":true,"stacks":3},"SequenceNode2WhentheLuceaniteGleamsHavocMaxed":{"isEnabled":true},"OutroSkillGentleTentacles":{"isEnabled":true},"RejuvenatingGlow":{"isEnabled":false},"MoonlitClouds":{"isEnabled":true},"ImpermanenceHeron":{"isEnabled":true},"MidnightVeil":{"isEnabled":true}}},"customBuffs":{"ATK":2,"ATK_FLAT":2,"HP":2,"HP_FLAT":2,"DEF":2,"DEF_FLAT":2,"CritRate":2,"CritDMG":2,"EnergyRegen":2,"BasicAttackDMGBonus":2,"HeavyAttackDMGBonus":2,"ResonanceSkillDMGBonus":2,"ResonanceLiberationDMGBonus":2,"Glacio":2,"Fusion":2,"Electro":2,"Aero":2,"Spectro":2,"Havoc":2,"HealingBonus":2,"CoordinatedDMGBonus":2,"EchoDMGBonus":2,"DamageAmplify":2,"ResistShred":2,"DefIgnore":2},"enemyLevel":"100","enemyResist":"0.2"},"Cartethyia":{"echoes":{"0":{"type":4,"echo":"ReminiscenceFleurdelys","echoSet":"WindwardPilgrimage","echoSubStatsType1":"CritRate","echoSubStatsValue1":8.1,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":16.2,"echoSubStatsType3":"HP","echoSubStatsValue3":8.6,"echoSubStatsType4":"BasicAttackDMGBonus","echoSubStatsValue4":8.6,"echoSubStatsType5":"HP_FLAT","echoSubStatsValue5":430,"stat":"CritRate"},"1":{"type":4,"echo":"NightmareKelpie","echoSet":"WindwardPilgrimage","stat":"CritDMG","echoSubStatsType1":"CritRate","echoSubStatsValue1":8.1,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":16.2,"echoSubStatsType3":"HP","echoSubStatsValue3":8.6,"echoSubStatsType4":"HP_FLAT","echoSubStatsValue4":430,"echoSubStatsType5":"BasicAttackDMGBonus","echoSubStatsValue5":8.6},"2":{"type":1,"echo":"DevoteesFlesh","echoSet":"WindwardPilgrimage","stat":"HP","echoSubStatsType1":"CritRate","echoSubStatsValue1":8.1,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":16.2,"echoSubStatsType3":"EnergyRegen","echoSubStatsValue3":8.4,"echoSubStatsType4":"DEF","echoSubStatsValue4":10.9,"echoSubStatsType5":"DEF_FLAT","echoSubStatsValue5":50},"3":{"type":1,"echo":"FusionDrake","echoSet":"WindwardPilgrimage","stat":"HP","echoSubStatsType1":"CritRate","echoSubStatsValue1":8.1,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":16.2,"echoSubStatsType3":"EnergyRegen","echoSubStatsValue3":8.4,"echoSubStatsType4":"DEF","echoSubStatsValue4":10.9,"echoSubStatsType5":"DEF_FLAT","echoSubStatsValue5":50},"4":{"type":1,"echo":"SpectroDrake","echoSet":"WindwardPilgrimage","echoSubStatsType1":"CritRate","echoSubStatsValue1":8.1,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":16.2,"echoSubStatsType3":"DEF_FLAT","echoSubStatsValue3":50,"echoSubStatsType4":"DEF","echoSubStatsValue4":10.9,"echoSubStatsType5":"ATK","echoSubStatsValue5":8.6}},"mainEcho":{"echo":"ReminiscenceFleurdelys","rank":5,"isEnabled":true},"buffs":{"MandateofDivinity":{"isEnabled":true},"BladeofHowlingSquallAeroErosionStacks":{"isEnabled":true,"stacks":5},"InherentSkillAHeartsTruestWishes":{"isEnabled":true},"InherentSkillWindsIndelibleImprintFirstStacks":{"isEnabled":true},"InherentSkillWindsIndelibleImprintAdditionalStacks":{"isEnabled":true,"stacks":3},"StatBonusCritRate1":{"isEnabled":true},"StatBonusCritRate2":{"isEnabled":true},"StatBonusCritRate3":{"isEnabled":true},"StatBonusCritRate4":{"isEnabled":true},"StatBonusHP1":{"isEnabled":true},"StatBonusHP2":{"isEnabled":true},"StatBonusHP3":{"isEnabled":true},"StatBonusHP4":{"isEnabled":true}},"weapon":"DefiersThorn","weaponPassives":{"DefiersThornHP":{"isEnabled":true},"DefiersThornDEFIgnore":{"isEnabled":true},"DefiersThornAmplify":{"isEnabled":true}},"echoSetBonus":{"setBonusOne":"Windward Pilgrimage 2 Set","setBonusTwo":"Windward Pilgrimage 5 Set"},"echoSetPassives":{"WindwardPilgrimage2Set":{"isEnabled":true},"WindwardPilgrimage5SetCritRate":{"isEnabled":true},"WindwardPilgrimage5SetAero":{"isEnabled":true}},"resonanceChains":{"SequenceNode1CrownDestinedbyFate":{"isEnabled":true,"stacks":4},"SequenceNode2BladeBrokenbyTempest":{"isEnabled":true},"SequenceNode3PrisonerHangedintheTower":{"isEnabled":true},"SequenceNode4SacrificeMadeforSalvation":{"isEnabled":true},"SequenceNode5HopeReshapedinStorms":{"isEnabled":true},"SequenceNode6FreedomFoundinStormsWake":{"isEnabled":true}},"teamBuffs":{"selectedCharacter1":"Ciaccona","selectedCharacter2":"RoverAeroFemale","buffs":{"OutroSkillWindcallingTune":{"isEnabled":true},"SoloConcert":{"isEnabled":true},"SequenceNode2SongoftheFourSeasons":{"isEnabled":true},"SequenceNode5EternalIdylltoLastinSummer":{"isEnabled":true},"RejuvenatingGlow":{"isEnabled":true},"BellBorne eochelone":{"isEnabled":true},"GustsofWelkin":{"isEnabled":true},"BloodpactsPledgeAeroDeepen":{"isEnabled":true},"WoodlandAriaAeroShred":{"isEnabled":true}}},"rotations":[{"name":"Maygi\'s Fleurdelys Rotation","description":"","actions":[{"order":1,"key":"SwordtoMarkTidesTraceDMG","type":"intro","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"isDisabled":false,"id":"fmc9yll0xw"},{"order":2,"key":"SwordToAnswerWavesCallDMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"isDisabled":false,"id":"sn66bskez4"},{"order":3,"key":"MayTempestBreakTheTidesDMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"isDisabled":false,"id":"prmf5bp8j6"},{"order":4,"key":"BasicAttackStage3DMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"isDisabled":false,"id":"yhifipp8c1"},{"order":5,"key":"BasicAttackStage4DMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"isDisabled":false,"id":"rgj7qljyy5"},{"order":6,"key":"BasicAttackStage5DMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"isDisabled":false,"id":"ki800erahk"},{"order":7,"key":"Stage2DMG","type":"basic","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"isDisabled":false,"id":"h2syiepxt4"},{"order":8,"key":"Stage3DMG","type":"basic","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"isDisabled":false,"id":"ix696diazb"},{"order":9,"key":"Stage4DMG","type":"basic","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"isDisabled":false,"id":"ke1qv6p2lz"},{"order":10,"key":"BasicAttackStage2DMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"isDisabled":false,"id":"tvrwn4zr0p"},{"order":11,"key":"BasicAttackStage3DMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"isDisabled":false,"id":"ogychygw0q"},{"order":12,"key":"BasicAttackStage4DMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"isDisabled":false,"id":"i7hur43ilu"},{"order":13,"key":"BasicAttackStage5DMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"isDisabled":false,"id":"xofrszlpmk"},{"order":14,"key":"BladeofHowlingSquallDMG","type":"liberation","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"isDisabled":false,"id":"cfilsgic20"}],"id":"j4l1xwu2pa"}],"customBuffs":{"ATK":5,"ATK_FLAT":5,"HP":5,"HP_FLAT":5,"DEF":5,"DEF_FLAT":5,"CritRate":5,"CritDMG":5,"EnergyRegen":5,"BasicAttackDMGBonus":5,"HeavyAttackDMGBonus":5,"ResonanceSkillDMGBonus":5,"ResonanceLiberationDMGBonus":5,"Glacio":5,"Fusion":5,"Electro":5,"Aero":5,"Spectro":5,"Havoc":5,"HealingBonus":5,"CoordinatedDMGBonus":5,"EchoDMGBonus":5,"DamageAmplify":5,"ResistShred":5,"DefIgnore":5},"aeroErosionStacks":"6","enemyLevel":"100","enemyResist":"0.2"}},"activeCharacter":"Cartethyia"}',
+    inventory: null,
+  },
+};
+
+export const dataAttacks: AttackTests = [
+  {
+    selector: ".stage-1-dmg",
+    values: ["Stage 1 DMG", "8017", "21076", "22447"],
+  },
+  {
+    selector: ".stage-2-dmg",
+    values: ["Stage 2 DMG", "22021", "57893", "61658"],
+  },
+  {
+    selector: ".stage-3-dmg",
+    values: ["Stage 3 DMG", "28713", "75486", "80395"],
+  },
+  {
+    selector: ".stage-4-dmg",
+    values: ["Stage 4 DMG", "25325", "66579", "70910"],
+  },
+  {
+    selector: ".dodge-counter-dmg",
+    values: ["Dodge Counter DMG", "45954", "120812", "128670"],
+  },
+  {
+    selector: ".heavy-attack-dmg",
+    values: ["Heavy Attack DMG", "20931", "55027", "58606"],
+  },
+  {
+    selector: ".mid-air-attack-dmg",
+    values: ["Mid-air Attack DMG", "18952", "49824", "53065"],
+  },
+  {
+    selector: ".mid-air-attack-1-sword-shadow-recalled-dmg",
+    values: [
+      "Mid-air Attack 1 Sword Shadow Recalled DMG",
+      "32990",
+      "86731",
+      "92372",
+    ],
+  },
+  {
+    selector: ".mid-air-attack-2-sword-shadows-recalled-dmg",
+    values: [
+      "Mid-air Attack 2 Sword Shadows Recalled DMG",
+      "57806",
+      "151970",
+      "161855",
+    ],
+  },
+  {
+    selector: ".mid-air-attack-3-sword-shadows-recalleddmg",
+    values: [
+      "Mid-air Attack 3 Sword Shadows RecalledDMG",
+      "197764",
+      "519920",
+      "553738",
+    ],
+  },
+  {
+    selector: ".sequence5-hope-reshaped-in-storms-shield",
+    values: ["Sequence5: Hope Reshaped in Storms Shield", "11695"],
+  },
+  {
+    selector: ".sword-to-bear-their-names-dmg",
+    values: ["Sword to Bear Their Names DMG", "33018", "86802", "92448"],
+  },
+  {
+    selector: ".blade-of-howling-squall-dmg",
+    values: ["Blade of Howling Squall DMG", "476327", "1683339", "1810042"],
+  },
+  {
+    selector: ".basic-attack-stage-1-dmg",
+    values: ["Basic Attack Stage 1 DMG", "10159", "35902", "38604"],
+  },
+  {
+    selector: ".basic-attack-stage-2-dmg",
+    values: ["Basic Attack Stage 2 DMG", "14229", "50285", "54070"],
+  },
+  {
+    selector: ".basic-attack-stage-3-dmg",
+    values: ["Basic Attack Stage 3 DMG", "16671", "58915", "63349"],
+  },
+  {
+    selector: ".basic-attack-stage-4-dmg",
+    values: ["Basic Attack Stage 4 DMG", "21445", "75787", "81491"],
+  },
+  {
+    selector: ".basic-attack-stage-5-dmg",
+    values: ["Basic Attack Stage 5 DMG", "56352", "199147", "214136"],
+  },
+  {
+    selector: ".dodge-counter-dmg",
+    values: ["Dodge Counter DMG", "25030", "88455", "95112"],
+  },
+  {
+    selector: ".upward-cut-dmg",
+    values: ["Upward Cut DMG", "14214", "50230", "54010"],
+  },
+  {
+    selector: ".heavy-attack-dmg",
+    values: ["Heavy Attack DMG", "22306", "78829", "84763"],
+  },
+  {
+    selector: ".enhanced-heavy-attack-dmg",
+    values: ["Enhanced Heavy Attack DMG", "30446", "107595", "115693"],
+  },
+  {
+    selector: ".mid-air-attack-1-dmg",
+    values: ["Mid-air Attack 1 DMG", "14182", "50119", "53891"],
+  },
+  {
+    selector: ".mid-air-attack-2-dmg",
+    values: ["Mid-air Attack 2 DMG", "46256", "163466", "175770"],
+  },
+  {
+    selector: ".mid-air-attack-3-dmg",
+    values: ["Mid-air Attack 3 DMG", "3444", "12171", "13087"],
+  },
+  {
+    selector: ".sword-to-answer-waves-call-dmg",
+    values: ["Sword to Answer Waves' Call DMG", "36946", "130566", "140393"],
+  },
+  {
+    selector: ".may-tempest-break-the-tides-dmg",
+    values: ["May Tempest Break the Tides DMG", "36961", "130618", "140450"],
+  },
+  {
+    selector: ".sword-to-mark-tide-s-trace-dmg",
+    values: ["Sword to Mark Tide's Trace DMG", "19627", "51597", "54954"],
+  },
+  {
+    selector: ".sword-to-call-for-freedom-dmg",
+    values: ["Sword to Call for Freedom DMG", "20916", "73916", "79480"],
+  },
+  { selector: ".aero-erosion", values: ["Aero Erosion", "23974"] },
+  {
+    selector: ".windcleaver-dmg",
+    values: ["Windcleaver DMG", "9720", "25554", "27216"],
+  },
+  {
+    selector: ".sword-to-mark-tide-s-trace-dmg",
+    values: ["Sword to Mark Tide's Trace DMG", "19627", "51597", "54954"],
+  },
+  {
+    selector: ".sword-to-answer-waves-call-dmg",
+    values: ["Sword to Answer Waves' Call DMG", "36946", "130566", "140393"],
+  },
+  {
+    selector: ".may-tempest-break-the-tides-dmg",
+    values: ["May Tempest Break the Tides DMG", "36961", "130618", "140450"],
+  },
+  {
+    selector: ".basic-attack-stage-3-dmg",
+    values: ["Basic Attack Stage 3 DMG", "16671", "58915", "63349"],
+  },
+  {
+    selector: ".basic-attack-stage-4-dmg",
+    values: ["Basic Attack Stage 4 DMG", "21445", "75787", "81491"],
+  },
+  {
+    selector: ".basic-attack-stage-5-dmg",
+    values: ["Basic Attack Stage 5 DMG", "56352", "199147", "214136"],
+  },
+  {
+    selector: ".stage-2-dmg",
+    values: ["Stage 2 DMG", "22021", "57893", "61658"],
+  },
+  {
+    selector: ".stage-3-dmg",
+    values: ["Stage 3 DMG", "28713", "75486", "80395"],
+  },
+  {
+    selector: ".stage-4-dmg",
+    values: ["Stage 4 DMG", "25325", "66579", "70910"],
+  },
+  {
+    selector: ".basic-attack-stage-2-dmg",
+    values: ["Basic Attack Stage 2 DMG", "14229", "50285", "54070"],
+  },
+  {
+    selector: ".basic-attack-stage-3-dmg",
+    values: ["Basic Attack Stage 3 DMG", "16671", "58915", "63349"],
+  },
+  {
+    selector: ".basic-attack-stage-4-dmg",
+    values: ["Basic Attack Stage 4 DMG", "21445", "75787", "81491"],
+  },
+  {
+    selector: ".basic-attack-stage-5-dmg",
+    values: ["Basic Attack Stage 5 DMG", "56352", "199147", "214136"],
+  },
+  {
+    selector: ".blade-of-howling-squall-dmg",
+    values: ["Blade of Howling Squall DMG", "476327", "1683339", "1810042"],
+  },
+];
+export const dataStats: StatTests = [
+  { selector: ".stat-atk", value: "1,236" },
+  { selector: ".stat-hp", value: "46,776" },
+  { selector: ".stat-def", value: "996" },
+  { selector: ".stat-cr", value: "90.5%" },
+  { selector: ".stat-cd", value: "280.0%" },
+  { selector: ".stat-er", value: "121.8%" },
+  { selector: ".stat-basic", value: "22.2%" },
+  { selector: ".stat-heavy", value: "5.0%" },
+  { selector: ".stat-skill", value: "5.0%" },
+  { selector: ".stat-liberation", value: "5.0%" },
+  { selector: ".stat-glacio", value: "25.0%" },
+  { selector: ".stat-fusion", value: "25.0%" },
+  { selector: ".stat-electro", value: "25.0%" },
+  { selector: ".stat-aero", value: "164.0%" },
+  { selector: ".stat-spectro", value: "25.0%" },
+  { selector: ".stat-havoc", value: "25.0%" },
+  { selector: ".stat-healing", value: "5.0%" },
+];
