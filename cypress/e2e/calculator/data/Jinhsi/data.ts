0788f9c75b20892a7ec76df6bf36b17a38f8b334
Ryan Benson <benson.ryan@gmail.com>
2025-09-16 11:49:00 -0500

    Adds jiyan and jihnsi e2e tests

---
+++ cypress/e2e/calculator/data/Jinhsi/data.ts
@@ -0,0 +1,162 @@
+export const config = 
+    {"meta":{"version":"2","source":"WutheringTools"},"data":{"character":"{\"characters\":{\"Augusta\":{\"echoes\":{\"0\":{},\"1\":{},\"2\":{},\"3\":{},\"4\":{}},\"mainEcho\":{\"echo\":null,\"rank\":5}},\"Jinhsi\":{\"echoes\":{\"0\":{\"type\":4,\"echo\":\"Jué\",\"echoSet\":\"CelestialLight\",\"stat\":\"CritDMG\",\"echoSubStatsType1\":\"CritRate\",\"echoSubStatsValue1\":8.1,\"echoSubStatsType2\":\"CritDMG\",\"echoSubStatsValue2\":16.2,\"echoSubStatsType3\":\"ATK\",\"echoSubStatsValue3\":8.6,\"echoSubStatsType4\":\"ATK_FLAT\",\"echoSubStatsValue4\":50,\"echoSubStatsType5\":\"ResonanceSkillDMGBonus\",\"echoSubStatsValue5\":8.6},\"1\":{\"type\":3,\"echo\":\"CyanFeatheredHeron\",\"echoSet\":\"CelestialLight\",\"stat\":\"Spectro\",\"echoSubStatsType1\":\"CritRate\",\"echoSubStatsValue1\":8.1,\"echoSubStatsType2\":\"CritDMG\",\"echoSubStatsValue2\":16.2,\"echoSubStatsType3\":\"ATK\",\"echoSubStatsValue3\":8.6,\"echoSubStatsType4\":\"ATK_FLAT\",\"echoSubStatsValue4\":50,\"echoSubStatsType5\":\"ResonanceSkillDMGBonus\",\"echoSubStatsValue5\":8.6},\"2\":{\"type\":3,\"echo\":\"Lightcrusher\",\"echoSet\":\"CelestialLight\",\"stat\":\"Spectro\",\"echoSubStatsType1\":\"CritRate\",\"echoSubStatsValue1\":8.1,\"echoSubStatsType2\":\"CritDMG\",\"echoSubStatsValue2\":16.2,\"echoSubStatsType3\":\"EnergyRegen\",\"echoSubStatsValue3\":8.4,\"echoSubStatsType4\":\"HP_FLAT\",\"echoSubStatsValue4\":430,\"echoSubStatsType5\":\"DEF_FLAT\",\"echoSubStatsValue5\":60},\"3\":{\"type\":1,\"echo\":\"GlacioPredator\",\"echoSet\":\"CelestialLight\",\"stat\":\"ATK\",\"echoSubStatsType1\":\"CritRate\",\"echoSubStatsValue1\":8.1,\"echoSubStatsType2\":\"CritDMG\",\"echoSubStatsValue2\":16.2,\"echoSubStatsType3\":\"EnergyRegen\",\"echoSubStatsValue3\":8.4,\"echoSubStatsType4\":\"DEF\",\"echoSubStatsValue4\":10.9,\"echoSubStatsType5\":\"HP\",\"echoSubStatsValue5\":8.6},\"4\":{\"type\":1,\"echo\":\"SpectroPrism\",\"echoSet\":\"CelestialLight\",\"stat\":\"ATK\",\"echoSubStatsType1\":\"CritRate\",\"echoSubStatsValue1\":8.1,\"echoSubStatsType2\":\"CritDMG\",\"echoSubStatsValue2\":16.2,\"echoSubStatsType3\":\"EnergyRegen\",\"echoSubStatsValue3\":8.4,\"echoSubStatsType4\":\"HP\",\"echoSubStatsValue4\":8.6,\"echoSubStatsType5\":\"DEF\",\"echoSubStatsValue5\":10.9}},\"mainEcho\":{\"echo\":\"Jué\",\"rank\":5,\"isEnabled\":true},\"buffs\":{\"Incandescence\":{\"isEnabled\":true,\"stacks\":50},\"InherentSkillRadiantSurge\":{\"isEnabled\":true},\"InherentSkillConvergedFlash\":{\"isEnabled\":true},\"StatBonusCritRate1\":{\"isEnabled\":true},\"StatBonusCritRate2\":{\"isEnabled\":true},\"StatBonusATK1\":{\"isEnabled\":true},\"StatBonusATK2\":{\"isEnabled\":true},\"StatBonusCritRate3\":{\"isEnabled\":true},\"StatBonusCritRate4\":{\"isEnabled\":true},\"StatBonusATK3\":{\"isEnabled\":true},\"StatBonusATK4\":{\"isEnabled\":true}},\"weapon\":\"AgesOfHarvest\",\"weaponPassives\":{\"AgesofHarvestAllElementAttributeBonus\":{\"isEnabled\":true},\"AgesofHarvestResonanceSkillDMGBonus\":{\"stacks\":2,\"isEnabled\":true}},\"echoSetBonus\":{\"setBonusOne\":\"Celestial Light 2 Set\",\"setBonusTwo\":\"Celestial Light 5 Set\"},\"echoSetPassives\":{\"CelestialLight2SetSpectro\":{\"isEnabled\":true},\"CelestialLight5SetSpectro\":{\"isEnabled\":true}},\"resonanceChains\":{\"SequenceNode1AbyssalAscension\":{\"isEnabled\":true,\"stacks\":4},\"SequenceNode2ChronofrostRepose\":{\"isEnabled\":true},\"SequenceNode3CelestialIncarnate\":{\"isEnabled\":true,\"stacks\":2},\"SequenceNode4BenevolentGrace\":{\"isEnabled\":true},\"SequenceNode5FrostfireIllumination\":{\"isEnabled\":true},\"SequenceNode6ComesSpringwhenChillExhausts1\":{\"isEnabled\":true}},\"teamBuffs\":{\"selectedCharacter1\":\"Cantarella\",\"selectedCharacter2\":\"Verina\",\"buffs\":{\"OutroSkillGentleTentacles\":{\"isEnabled\":true},\"InherentSkillGiftofNature\":{\"isEnabled\":true},\"OutroSkillBlossom\":{\"isEnabled\":true},\"SequenceNode4BlossomingEmbrace\":{\"isEnabled\":true},\"RejuvenatingGlow\":{\"isEnabled\":true},\"MoonlitClouds\":{\"isEnabled\":true},\"ImpermanenceHeron\":{\"isEnabled\":true},\"BellBorne eochelone\":{\"isEnabled\":true}}},\"rotations\":[{\"name\":\"TacticalKush was here :3\",\"description\":\"\",\"actions\":[{\"type\":\"intro\",\"order\":1,\"count\":1,\"buffs\":[{\"modifier\":null,\"modifierValue\":null,\"id\":\"ax7kqzklvf\"}],\"key\":\"LoongsHalo\",\"id\":\"wrjjulfu2p\"},{\"order\":2,\"key\":\"OverflowingRadianceDMG\",\"type\":\"skill\",\"count\":1,\"buffs\":[{\"modifier\":null,\"modifierValue\":null,\"id\":\"fybuto1mky\"}],\"id\":\"s6j9bv29de\"},{\"order\":3,\"key\":\"CrescentDivinityDMG\",\"type\":\"forteCircuit\",\"count\":1,\"buffs\":[],\"id\":\"vljgiw6fez\"},{\"order\":4,\"key\":\"BasicAttack1DMG\",\"type\":\"forteCircuit\",\"count\":\"1\",\"buffs\":[],\"id\":\"vk7xh30hpz\"},{\"order\":5,\"key\":\"BasicAttack2DMG\",\"type\":\"forteCircuit\",\"count\":1,\"buffs\":[],\"id\":\"qz3y4avruy\"},{\"order\":6,\"key\":\"BasicAttack3DMG\",\"type\":\"forteCircuit\",\"count\":1,\"buffs\":[],\"id\":\"8dqujeyr26\"},{\"order\":7,\"key\":\"BasicAttack4DMG\",\"type\":\"forteCircuit\",\"count\":1,\"buffs\":[],\"id\":\"wjufthwe35\"},{\"order\":8,\"key\":\"SolarFlareDMG\",\"type\":\"forteCircuit\",\"count\":1,\"buffs\":[{\"modifier\":null,\"modifierValue\":null,\"id\":\"l4mcuxzhk1\"}],\"id\":\"zk8y5gjpii\"},{\"order\":9,\"key\":\"StarGlamourDMG\",\"type\":\"forteCircuit\",\"count\":1,\"buffs\":[],\"id\":\"gu5d11343w\"},{\"order\":10,\"key\":\"PurgeOfLight\",\"type\":\"liberation\",\"count\":1,\"buffs\":[],\"id\":\"oofspyt1ia\"}],\"id\":\"rvowurnbqp\"}],\"customBuffs\":{\"ATK\":5,\"ATK_FLAT\":5,\"HP\":5,\"HP_FLAT\":5,\"DEF\":5,\"DEF_FLAT\":5,\"CritRate\":5,\"CritDMG\":5,\"EnergyRegen\":5,\"BasicAttackDMGBonus\":5,\"HeavyAttackDMGBonus\":5,\"ResonanceSkillDMGBonus\":5,\"ResonanceLiberationDMGBonus\":5,\"Glacio\":5,\"Fusion\":5,\"Electro\":5,\"Aero\":5,\"Spectro\":5,\"Havoc\":5,\"HealingBonus\":5,\"CoordinatedDMGBonus\":5,\"EchoDMGBonus\":5,\"DamageAmplify\":5,\"ResistShred\":5,\"DefIgnore\":5},\"enemyLevel\":\"100\",\"enemyResist\":\"0.2\"}},\"activeCharacter\":\"Jinhsi\"}","inventory":""}};
+
+export const dataAttacks: AttackTests = [
+  { selector: ".stage-1-dmg", values: ["Stage 1 DMG", "3282", "8172", "9188"] },
+  {
+    selector: ".stage-2-dmg",
+    values: ["Stage 2 DMG", "4813", "11986", "13476"],
+  },
+  {
+    selector: ".stage-3-dmg",
+    values: ["Stage 3 DMG", "5257", "13092", "14720"],
+  },
+  {
+    selector: ".stage-4-dmg",
+    values: ["Stage 4 DMG", "7786", "19391", "21801"],
+  },
+  {
+    selector: ".heavy-attack-dmg",
+    values: ["Heavy Attack DMG", "11779", "29334", "32981"],
+  },
+  {
+    selector: ".mid-air-attack-dmg",
+    values: ["Mid-air Attack DMG", "6086", "15157", "17041"],
+  },
+  {
+    selector: ".dodge-counter-dmg",
+    values: ["Dodge Counter DMG", "7246", "18046", "20289"],
+  },
+  {
+    selector: ".trailing-lights-of-eons-dmg",
+    values: ["Trailing Lights of Eons DMG", "11809", "29408", "33064"],
+  },
+  {
+    selector: ".overflowing-radiance-dmg",
+    values: ["Overflowing Radiance DMG", "14965", "37268", "41901"],
+  },
+  {
+    selector: ".purge-of-light-dmg",
+    values: ["Purge of Light DMG", "180939", "450611", "506629"],
+  },
+  {
+    selector: ".incarnation-basic-attack-1-dmg",
+    values: ["Incarnation - Basic Attack 1 DMG", "6722", "16740", "18822"],
+  },
+  {
+    selector: ".incarnation-basic-attack-2-dmg",
+    values: ["Incarnation - Basic Attack 2 DMG", "9857", "24548", "27599"],
+  },
+  {
+    selector: ".incarnation-basic-attack-3-dmg",
+    values: ["Incarnation - Basic Attack 3 DMG", "12572", "31308", "35200"],
+  },
+  {
+    selector: ".incarnation-basic-attack-4-dmg",
+    values: ["Incarnation - Basic Attack 4 DMG", "14161", "35266", "39650"],
+  },
+  {
+    selector: ".incarnation-heavy-attack-dmg",
+    values: ["Incarnation - Heavy Attack DMG", "7853", "19555", "21986"],
+  },
+  {
+    selector: ".incarnation-dodge-counter-dmg",
+    values: ["Incarnation - Dodge Counter DMG", "10833", "26979", "30332"],
+  },
+  {
+    selector: ".crescent-divinity-dmg",
+    values: ["Crescent Divinity DMG", "38214", "95166", "106997"],
+  },
+  {
+    selector: ".illuminous-epiphany-solar-flare-dmg",
+    values: ["Illuminous Epiphany: Solar Flare DMG", "15888", "39566", "44484"],
+  },
+  {
+    selector: ".illuminous-epiphany-stella-glamor-dmg",
+    values: [
+      "Illuminous Epiphany: Stella Glamor DMG",
+      "342784",
+      "853670",
+      "959795",
+    ],
+  },
+  {
+    selector: ".loong-s-halo-dmg",
+    values: ["Loong's Halo DMG", "11581", "28841", "32426"],
+  },
+  { selector: ".summon-dmg", values: ["Summon DMG", "2402", "5980", "6724"] },
+  {
+    selector: ".thunderbolt-dmg",
+    values: ["Thunderbolt DMG", "4804", "11963", "13450"],
+  },
+  { selector: ".spiral-dmg", values: ["Spiral DMG", "4803", "11960", "13447"] },
+  {
+    selector: ".coordinated-dmg",
+    values: ["Coordinated DMG", "1230", "3063", "3443"],
+  },
+  {
+    selector: ".loong-s-halo-dmg",
+    values: ["Loong's Halo DMG", "11581", "28841", "32426"],
+  },
+  {
+    selector: ".overflowing-radiance-dmg",
+    values: ["Overflowing Radiance DMG", "14965", "37268", "41901"],
+  },
+  {
+    selector: ".crescent-divinity-dmg",
+    values: ["Crescent Divinity DMG", "38214", "95166", "106997"],
+  },
+  {
+    selector: ".incarnation-basic-attack-1-dmg",
+    values: ["Incarnation - Basic Attack 1 DMG", "6722", "16740", "18822"],
+  },
+  {
+    selector: ".incarnation-basic-attack-2-dmg",
+    values: ["Incarnation - Basic Attack 2 DMG", "9857", "24548", "27599"],
+  },
+  {
+    selector: ".incarnation-basic-attack-3-dmg",
+    values: ["Incarnation - Basic Attack 3 DMG", "12572", "31308", "35200"],
+  },
+  {
+    selector: ".incarnation-basic-attack-4-dmg",
+    values: ["Incarnation - Basic Attack 4 DMG", "14161", "35266", "39650"],
+  },
+  {
+    selector: ".illuminous-epiphany-solar-flare-dmg",
+    values: ["Illuminous Epiphany: Solar Flare DMG", "15888", "39566", "44484"],
+  },
+  {
+    selector: ".illuminous-epiphany-stella-glamor-dmg",
+    values: [
+      "Illuminous Epiphany: Stella Glamor DMG",
+      "342784",
+      "853670",
+      "959795",
+    ],
+  },
+  {
+    selector: ".purge-of-light-dmg",
+    values: ["Purge of Light DMG", "180939", "450611", "506629"],
+  },
+];
+
+export const dataStats: StatTests = [
+    { selector: ".stat-atk", value: "3,229" },
+    { selector: ".stat-hp", value: "18,223" },
+    { selector: ".stat-def", value: "1,660" },
+    { selector: ".stat-cr", value: "82.8%" },
+    { selector: ".stat-cd", value: "280.0%" },
+    { selector: ".stat-er", value: "130.2%" },
+    { selector: ".stat-basic", value: "5.0%" },
+    { selector: ".stat-heavy", value: "5.0%" },
+    { selector: ".stat-skill", value: "86.2%" },
+    { selector: ".stat-liberation", value: "5.0%" },
+    { selector: ".stat-glacio", value: "37.0%" },
+    { selector: ".stat-fusion", value: "37.0%" },
+    { selector: ".stat-electro", value: "37.0%" },
+    { selector: ".stat-aero", value: "37.0%" },
+    { selector: ".stat-spectro", value: "172.0%" },
+    { selector: ".stat-havoc", value: "37.0%" },
+    { selector: ".stat-healing", value: "5.0%" },
+];
