ad6c8d29abf67d6ea8453c4c652726d366167708
Ryan Benson <benson.ryan@gmail.com>
2025-10-05 09:21:08 -0500

    Updates

--- src/pages/UpdatesView.vue
+++ src/pages/UpdatesView.vue
@@ -10,6 +10,10 @@
       <li>More!</li>
     </ul>
     <h1>Updates</h1>
+    <h3>October 5, 2025</h3>
+    <ul>
+      <li>Adds ability to exclude equipped echoes when optimizing.</li>
+    </ul>
     <h3>October 3, 2025</h3>
     <ul>
       <li>
