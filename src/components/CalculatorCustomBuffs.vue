e2291f440775295ea042a298fce9a6ef6e1371b4
Ryan Benson <benson.ryan@gmail.com>
2025-10-09 10:37:23 -0500

    Update CalculatorCustomBuffs.vue
    Removes min = 0 on custom buffs

--- src/components/CalculatorCustomBuffs.vue
+++ src/components/CalculatorCustomBuffs.vue
@@ -20,7 +20,7 @@
         <input
           type="number"
           v-model="ATK"
-          min="0"
+          
           name="ATK"
           id="ATK"
           class="input input-md input-bordered"
@@ -38,7 +38,7 @@
         <input
           type="number"
           v-model="ATK_FLAT"
-          min="0"
+          
           name="ATK_FLAT"
           id="ATK_FLAT"
           class="input input-md input-bordered"
@@ -56,7 +56,7 @@
         <input
           type="number"
           v-model="HP"
-          min="0"
+          
           name="HP"
           id="HP"
           class="input input-md input-bordered"
@@ -74,7 +74,7 @@
         <input
           type="number"
           v-model="HP_FLAT"
-          min="0"
+          
           name="HP_FLAT"
           id="HP_FLAT"
           class="input input-md input-bordered"
@@ -92,7 +92,7 @@
         <input
           type="number"
           v-model="DEF"
-          min="0"
+          
           name="DEF"
           id="DEF"
           class="input input-md input-bordered"
@@ -110,7 +110,7 @@
         <input
           type="number"
           v-model="DEF_FLAT"
-          min="0"
+          
           name="DEF_FLAT"
           id="DEF_FLAT"
           class="input input-md input-bordered"
@@ -128,7 +128,7 @@
         <input
           type="number"
           v-model="CritRate"
-          min="0"
+          
           name="CritRate"
           id="CritRate"
           class="input input-md input-bordered"
@@ -146,7 +146,7 @@
         <input
           type="number"
           v-model="CritDMG"
-          min="0"
+          
           name="CritDMG"
           id="CritDMG"
           class="input input-md input-bordered"
@@ -163,7 +163,7 @@
         <input
           type="number"
           v-model="EnergyRegen"
-          min="0"
+          
           name="EnergyRegen"
           id="EnergyRegen"
           class="input input-md input-bordered"
@@ -180,7 +180,7 @@
         <input
           type="number"
           v-model="BasicAttackDMGBonus"
-          min="0"
+          
           name="BasicAttackDMGBonus"
           id="BasicAttackDMGBonus"
           class="input input-md input-bordered"
@@ -197,7 +197,7 @@
         <input
           type="number"
           v-model="HeavyAttackDMGBonus"
-          min="0"
+          
           name="HeavyAttackDMGBonus"
           id="HeavyAttackDMGBonus"
           class="input input-md input-bordered"
@@ -214,7 +214,7 @@
         <input
           type="number"
           v-model="ResonanceSkillDMGBonus"
-          min="0"
+          
           name="ResonanceSkillDMGBonus"
           id="ResonanceSkillDMGBonus"
           class="input input-md input-bordered"
@@ -231,7 +231,7 @@
         <input
           type="number"
           v-model="ResonanceLiberationDMGBonus"
-          min="0"
+          
           name="ResonanceLiberationDMGBonus"
           id="ResonanceLiberationDMGBonus"
           class="input input-md input-bordered"
@@ -249,7 +249,7 @@
         <input
           type="number"
           v-model="Glacio"
-          min="0"
+          
           name="Glacio"
           id="Glacio"
           class="input input-md input-bordered"
@@ -267,7 +267,7 @@
         <input
           type="number"
           v-model="Fusion"
-          min="0"
+          
           name="Fusion"
           id="Fusion"
           class="input input-md input-bordered"
@@ -285,7 +285,7 @@
         <input
           type="number"
           v-model="Electro"
-          min="0"
+          
           name="Electro"
           id="Electro"
           class="input input-md input-bordered"
@@ -303,7 +303,7 @@
         <input
           type="number"
           v-model="Aero"
-          min="0"
+          
           name="Aero"
           id="Aero"
           class="input input-md input-bordered"
@@ -321,7 +321,7 @@
         <input
           type="number"
           v-model="Spectro"
-          min="0"
+          
           name="Spectro"
           id="Spectro"
           class="input input-md input-bordered"
@@ -339,7 +339,7 @@
         <input
           type="number"
           v-model="Havoc"
-          min="0"
+          
           name="Havoc"
           id="Havoc"
           class="input input-md input-bordered"
@@ -356,7 +356,7 @@
         <input
           type="number"
           v-model="HealingBonus"
-          min="0"
+          
           name="HealingBonus"
           id="HealingBonus"
           class="input input-md input-bordered"
@@ -367,7 +367,7 @@
         <input
           type="number"
           v-model="CoordinatedDMGBonus"
-          min="0"
+          
           name="CoordinatedDMGBonus"
           id="CoordinatedDMGBonus"
           class="input input-md input-bordered"
@@ -378,7 +378,7 @@
         <input
           type="number"
           v-model="EchoDMGBonus"
-          min="0"
+          
           name="EchoDMGBonus"
           id="EchoDMGBonus"
           class="input input-md input-bordered"
@@ -393,7 +393,7 @@
         <input
           type="number"
           v-model="DamageAmplify"
-          min="0"
+          
           name="DamageAmplify"
           id="DamageAmplify"
           class="input input-md input-bordered"
@@ -408,7 +408,7 @@
         <input
           type="number"
           v-model="ResistShred"
-          min="0"
+          
           name="ResistShred"
           id="ResistShred"
           class="input input-md input-bordered"
@@ -423,7 +423,7 @@
         <input
           type="number"
           v-model="DefIgnore"
-          min="0"
+          
           name="DefIgnore"
           id="DefIgnore"
           class="input input-md input-bordered"
