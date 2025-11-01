2caf5f05f45a0ae4c2fac32690c19f3ae0c56b8d
unknown <benson.ryan@gmail.com>
2025-11-01 10:22:50 -0500

    Fixes QY s2

--- src/pages/UpdatesView.vue
+++ src/pages/UpdatesView.vue
@@ -10,6 +10,10 @@
       <li>More!</li>
     </ul>
     <h1>Updates</h1>
+    <h3>November 1, 2025</h3>
+    <ul>
+      <li>Fixes Qiuyuan's s2.</li>
+    </ul>
     <h3>October 31, 2025</h3>
     <ul>
       <li>Fixes Nightmare Kelpie Outro DMG, it is now properly doing Aero DMG.</li>
