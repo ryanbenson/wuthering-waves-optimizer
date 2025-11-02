046c0564c34de80e3a2ac7f84c7a7c5630e42055
unknown <benson.ryan@gmail.com>
2025-11-02 09:04:01 -0600

    Styles and uses range for rank

--- src/components/CalculatorDamage.vue
+++ src/components/CalculatorDamage.vue
@@ -57,6 +57,11 @@
       <td class="flex items-center gap-2">
         <img v-if="mainEchoImage" :src="mainEchoImage" class="size-6 rounded-full border border-solid neutral-content" />
         <span>{{ label }}</span>
+        <template v-if="isEchoAttack">
+          <span v-if="!isEquippedEchoSameAsActionEcho" class="mismatch-echo" v-tooltip="'This echo does not match your equipped main echo'">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" class="size-4"><path d="M320 64C334.7 64 348.2 72.1 355.2 85L571.2 485C577.9 497.4 577.6 512.4 570.4 524.5C563.2 536.6 550.1 544 536 544L104 544C89.9 544 76.8 536.6 69.6 524.5C62.4 512.4 62.1 497.4 68.8 485L284.8 85C291.8 72.1 305.3 64 320 64zM320 416C302.3 416 288 430.3 288 448C288 465.7 302.3 480 320 480C337.7 480 352 465.7 352 448C352 430.3 337.7 416 320 416zM320 224C301.8 224 287.3 239.5 288.6 257.7L296 361.7C296.9 374.2 307.4 384 319.9 384C332.5 384 342.9 374.3 343.8 361.7L351.2 257.7C352.5 239.5 338.1 224 319.8 224z"/></svg>
+          </span>
+        </template>
       </td>
       <td
         v-tooltip="{
@@ -91,6 +96,8 @@
 import { displayDamage } from "../utils/numbers";
 import { slugify } from "../utils/strings";
 import { getEchoData } from "../echoes";
+import { mapActions, mapState } from "pinia";
+import { useCharacterStore } from "../stores/character";
 export default {
   props: {
     character: {
@@ -131,6 +138,17 @@ export default {
     slugify,
   },
   computed: {
+    ...mapState(useCharacterStore, ["characters"]),
+    /**
+     * The current character data
+     * @returns {Object}
+     */
+    currentCharacter() {
+      return this.characters[this.character] ?? {};
+    },
+    currentCharacterMainEcho() {
+      return this.characters[this.character]?.mainEcho?.echo ?? null;
+    },
     slugifiedLabel() {
       return slugify(this.label) ?? "";
     },
@@ -152,11 +170,28 @@ export default {
     mainEchoImage() {
       return this.mainEchoData?.image ?? null;
     },
+    isEchoAttack() {
+      return this.mainEcho !== null;
+    },
+    isEquippedEchoSameAsActionEcho() {
+      if (!this.currentCharacterMainEcho || !this.mainEcho) {
+        return true;
+      }
+      return this.currentCharacterMainEcho === this.mainEcho;
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.mismatch-echo {
+  svg {
+    filter: none !important;
+  }
+  path {
+    fill: oklch(var(--wa));
+  }
+}
 .calculation__damage__item {
   display: grid;
   grid-template-columns: 1fr 100px 100px 100px;
--- src/components/CalculatorRotation.vue
+++ src/components/CalculatorRotation.vue
@@ -94,14 +94,19 @@
             :data-test-rotation-name-input="nameValue" />
 
           <div class="rotation__end">
-            <img
-              v-tooltip="currentEchoData?.name"
-              :src="
-                currentEchoData?.image ||
-                'https://ryanbenson.github.io/wuthering-waves-assets/images/echoes/monsters.png'
-              "
-              class="size-8 rounded-full border border-solid neutral-content bg-cover"
-              :alt="currentEchoData?.name" />
+            <div class="rotation__echo relative size-8">
+              <span v-if="!isEquippedEchoSameAsRotationEcho" class="mismatch-echo absolute top-[-8px] right-[-8px]" v-tooltip="'Rotation echo does not match your equipped main echo'">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" class="size-4"><path d="M320 64C334.7 64 348.2 72.1 355.2 85L571.2 485C577.9 497.4 577.6 512.4 570.4 524.5C563.2 536.6 550.1 544 536 544L104 544C89.9 544 76.8 536.6 69.6 524.5C62.4 512.4 62.1 497.4 68.8 485L284.8 85C291.8 72.1 305.3 64 320 64zM320 416C302.3 416 288 430.3 288 448C288 465.7 302.3 480 320 480C337.7 480 352 465.7 352 448C352 430.3 337.7 416 320 416zM320 224C301.8 224 287.3 239.5 288.6 257.7L296 361.7C296.9 374.2 307.4 384 319.9 384C332.5 384 342.9 374.3 343.8 361.7L351.2 257.7C352.5 239.5 338.1 224 319.8 224z"/></svg>
+              </span>
+              <img
+                v-tooltip="currentEchoData?.name"
+                :src="
+                  currentEchoData?.image ||
+                  'https://ryanbenson.github.io/wuthering-waves-assets/images/echoes/monsters.png'
+                "
+                class="size-8 rounded-full border border-solid neutral-content bg-cover"
+                :alt="currentEchoData?.name" />
+            </div>
             <div
               class="rotation__actions-count min-w-24"
               :data-test-rotation-actions-count="nameValue">
@@ -147,13 +152,23 @@
                   @click="openEchoChooser">
                   Choose echo
                 </button>
-                <input v-model="mainEchoRank" type="number" min="1" max="5" @input="onMainEchoRankChange"></input>
                 <button
                   class="btn btn-sm btn-outline btn-secondary"
                   @click="chooseCurrentMainEcho"
                   :disabled="!hasCurrentMainEcho">
                   Use current echo
                 </button>
+                <span class="main-echo--rank">
+                  <span class="font-bold">Rank</span> <span class="text-primary">{{ mainEchoRank }}</span>
+                </span>
+                <Range
+                  id="Rank"
+                  :values="[1, 2, 3, 4, 5]"
+                  :default-value="5"
+                  size="xs"
+                  class="w-full"
+                  @update-value="(val) => onMainEchoRankChange(val)"
+                  data-test-rotation-main-echo-rank="CritRate" />
               </div>
             </div>
             <div class="rotation__duration flex flex-col gap-2">
@@ -228,6 +243,7 @@
 <script>
 import { randomString } from "../utils/strings";
 import CalculatorRotationAction from "./CalculatorRotationAction.vue";
+import Range from "./Input/Range.vue";
 import { getEchoSetIconByType, echoSetLabelMap } from "../echoes/stats";
 import { mapActions, mapState } from "pinia";
 import { useCharacterStore } from "../stores/character";
@@ -277,6 +293,7 @@ export default {
   },
   components: {
     CalculatorRotationAction,
+    Range,
   },
   data() {
     return {
@@ -285,7 +302,7 @@ export default {
       descriptionValue: null,
       durationValue: null,
       echoValue: null,
-      mainEchoRank: 1,
+      mainEchoRank: 5,
       actionsList: [],
       // modal
       modalIdPicker: `echo-chooser-modal-${randomString()}`,
@@ -525,14 +542,15 @@ export default {
       this.echoValue = currentEchoKey;
       this.onEchoChange();
     },
-    onMainEchoRankChange(e) {
+    onMainEchoRankChange(val) {
+      this.mainEchoRank = val;
       this.$emit("updated-rotation", {
         id: this.id,
         name: this.nameValue,
         description: this.descriptionValue,
         duration: this.durationValue,
         echo: this.echoValue,
-        echoRank: e.target.value,
+        echoRank: val,
         actions: this.actionsList,
       });
     }
@@ -604,6 +622,12 @@ export default {
     hasCurrentMainEcho() {
       return !!this.currentCharacterMainEcho;
     },
+    isEquippedEchoSameAsRotationEcho() {
+      if (!this.currentCharacterMainEcho || !this.echoValue) {
+        return true;
+      }
+      return this.currentCharacterMainEcho === this.echoValue;
+    },
   },
   mounted() {
     const actions = JSON.parse(JSON.stringify(this.actions));
@@ -614,12 +638,20 @@ export default {
     this.descriptionValue = this.description;
     this.durationValue = this.duration || null;
     this.echoValue = this.echo || null;
-    this.mainEchoRank = this.echoRank || null;
+    this.mainEchoRank = this.echoRank || 5;
   },
 };
 </script>
 
 <style scoped lang="scss">
+.mismatch-echo {
+  svg {
+    filter: none !important;
+  }
+  path {
+    fill: oklch(var(--wa));
+  }
+}
 .echo-filters__sets--active {
   button {
     opacity: 0.6;
