c061912d21acf85d938dee1633a6048274759fb7
unknown <benson.ryan@gmail.com>
2025-11-09 11:28:15 -0600

    Adds ability to import echoes

--- src/pages/UpdatesView.vue
+++ src/pages/UpdatesView.vue
@@ -10,6 +10,10 @@
       <li>More!</li>
     </ul>
     <h1>Updates</h1>
+    <h3>November 9, 2025</h3>
+    <ul>
+      <li>Adds ability to import a list of echoes so they can be generated externally (e.g. scanner) and imported into your inventory..</li>
+    </ul>
     <h3>November 8, 2025</h3>
     <ul>
       <li>Fixes Rocksteady Guardian shield action.</li>
