a00a339bb8a4e373e4e5b3819eac7499da737730
Ryan Benson <benson.ryan@gmail.com>
2025-11-19 10:19:08 -0600

    Updates Chisa resonance chains
    Updated resonance chains for Chisa, modifying keys, names, details, and modifiers to enhance gameplay mechanics and balance.

--- src/characters/Chisa/resonanceChains.ts
+++ src/characters/Chisa/resonanceChains.ts
@@ -1,116 +1,17 @@
 export const resonanceChains = [
   {
-    key: "SequenceNode1SwordSheathedMindUnclouded",
-    name: "Sequence Node 1: Sword Sheathed, Mind Unclouded",
-    details: `<div><span class="ingame-Highlight">Thus Spoke the Blade: To Teach</span>, <span class="ingame-Highlight">Thus Spoke the Blade: To Save</span>, and <span class="ingame-Highlight">Thus Spoke the Blade: To Sacrifice</span> can no longer be interrupted.<br>Qiuyuan gains 20% Crit. Rate increase.</div>`,
-    hasStacks: false,
-    modifiers: [
-      {
-        modifier: "CritRate",
-        modifierValue: 0.2,
-      },
-    ],
-    minStacks: 0,
-    maxStacks: 0,
-    alwaysEnabled: false,
-  },
-  {
-    key: "SequenceNode2OBladeIWhoTeachNoMore",
-    name: "Sequence Node 2: O Blade, I, Who Teach No More",
-    details: `<span class="skilldescription">Bamboo's Shade now grants an additional 30% Echo Skill DMG Amplification to all nearby active Resonators in the team.</span>`,
-    hasStacks: false,
-    modifiers: [
-      {
-        modifier: "DMGDeepen:Echo",
-        modifierValue: 0.3,
-      },
-    ],
-    minStacks: 0,
-    maxStacks: 0,
-    alwaysEnabled: false,
-  },
-  {
-    key: "SequenceNode3OBladeIWhoSaveNoMore",
-    name: "Sequence Node 3: O Blade, I, Who Save No More",
-    details: `<div>The DMG Multiplier of Resonance Liberation <span class="ingame-Highlight">Sundering Strike</span> is increased by 500%.<br>If Concerto Energy is full when not in the Inksplash of Mind state, Qiuyuan's <span class="ingame-Highlight">Resonance Skill</span> is replaced with Resonance Skill <span class="ingame-Highlight">Straw Cape in Drizzly Rain</span>, available once every 20s.<br>Upon casting <span class="ingame-Highlight">Straw Cape in Drizzly Rain</span>, the <span class="ingame-Highlight">Quietude Within</span> effect immediately ends and 60 Concerto Energy is consumed to deal <span class="ingame-Wind">Aero DMG</span> equal to 500% of Qiuyuan's ATK, considered Echo Skill DMG, and restore 400 points of <a onclick="showTermExplan(141101,event)" class="ingame-term-desc">Swordster's Soliloquy</a>. The next <span class="ingame-Highlight">Basic Attack</span> is replaced with <span class="ingame-Highlight">Thus Spoke the Blade: Inkwash</span> Stage 3. When not in Co-op mode, upon casting <span class="ingame-Highlight">Straw Cape in Drizzly Rain</span>, the next <span class="ingame-Highlight">Outro Skill</span> is replaced with Outro Skill <span class="ingame-Highlight">Sheath Fallen, New Shoots Revealed</span>, which deals <span class="ingame-Wind">Aero DMG</span> equal to 500% of Qiuyuan's ATK, considered Echo Skill DMG.</saptag=5></div>`,
-    hasStacks: false,
-    modifiers: [
-      {
-        modifier: "talentModifierMultiplyAdd",
-        modifySpecificTalents: ["SunderingStrikeDMG"],
-        modifierValue: 5,
-      },
-      {
-        modifier: "EnableAttack",
-        modifierValue: [
-          "SequenceNode3OBladeIWhoSaveNoMore",
-          "SequenceNode3OBladeIWhoSaveNoMore2",
-        ],
-      },
-    ],
-    minStacks: 0,
-    maxStacks: 0,
-    alwaysEnabled: false,
-  },
-  {
-    key: "SequenceNode3OBladeIWhoSaveNoMoreInksplashofMind",
-    name: "Sequence Node 3: O Blade, I, Who Save No More - Inksplash of Mind",
-    details: `<div>Upon casting <span class="ingame-Highlight">Straw Cape in Drizzly Rain</span>, Qiuyuan cannot gain the <span class="ingame-Highlight">Quietude Within</span> effect the next time he enters <a onclick="showTermExplan(141102,event)" class="ingame-term-desc">Inksplash of Mind</a>. <span class="ingame-Highlight">Thus Spoke the Blade: To Teach</span>, <span class="ingame-Highlight">Thus Spoke the Blade: To Save</span>, and <span class="ingame-Highlight">Thus Spoke the Blade: To Sacrifice</span> gain 600% DMG Multiplier increase and additionally restore <saptag=5>30 points of Concerto Energy on hit.<br></div>`,
-    hasStacks: false,
-    modifiers: [
-      {
-        modifier: "talentModifierMultiplyAdd",
-        modifySpecificTalents: [
-          "ThusSpoketheBladeToTeachDMG",
-          "ThusSpoketheBladeToSaveDMG",
-          "ThusSpoketheBladeToSacrificeDMG",
-        ],
-        modifierValue: 6,
-      },
-    ],
-    minStacks: 0,
-    maxStacks: 0,
-    alwaysEnabled: false,
-  },
-  {
-    key: "SequenceNode4OBladeIWhoSacrificeNoMore",
-    name: "Sequence Node 4: O Blade, I, Who Sacrifice No More",
-    details: `<span class="skilldescription">ATK is increased by 20%.</span>`,
+    key: "SequenceNode1WanderingThroughtheDesolateCorridors",
+    name: "Sequence Node 1: Wandering Through the Desolate Corridors",
+    details: `<div>Chisa is immune to interruption during <span class="Highlight"><strong>Sawring - Blitz</strong></span>,  <span class="Highlight"><strong>Sawring - Eradication</strong></span>, and <span class="Highlight"><strong>Chainsaw Mode - Dodge Counter</strong></span><br><br>Inflicting <span class="Highlight"><strong>Unseen Snare</strong></span> grants the following additional effects:<br>- Chisa's ATK is increased by 30% for 15s.<br>- Deal fixed 61803 points of <span class="Dark"><strong>Havoc DMG</strong></span>. The target's HP can be reduced to 61.80% at most and each target can take this damage only once. This instance of damage is considered Basic Attack DMG that does not bear any effect from damage bonuses.</div>`,
     hasStacks: false,
     modifiers: [
       {
         modifier: "ATK",
-        modifierValue: 0.2,
+        modifierValue: 0.3,
       },
-    ],
-    minStacks: 0,
-    maxStacks: 0,
-    alwaysEnabled: false,
-  },
-  {
-    key: "SequenceNode5OBladeIWhoAwaittobeWielded",
-    name: "Sequence Node 5: O Blade, I, Who Await to be Wielded",
-    details: `<span class="skilldescription">Qiuyuan now ignores 15% of the target's DEF when dealing damage.</span>`,
-    hasStacks: false,
-    modifiers: [
-      {
-        modifier: "DEFIgnore",
-        modifierValue: 0.15,
-      },
-    ],
-    minStacks: 0,
-    maxStacks: 0,
-    alwaysEnabled: false,
-  },
-  {
-    key: "SequenceNode6ThusIHeardThusISawThusISpoke",
-    name: "Sequence Node 6: Thus I Heard, Thus I Saw, Thus I Spoke",
-    details: `<div>Casting Heavy Attack <span class="ingame-Highlight">Thus Spoke the Blade: To Sacrifice</span> stagnates nearby targets for 3s or until the targets are damaged or until Qiuyuan is switched off the field.<br>When Qiuyuan is the active Resonator in the team, upon exiting <a onclick="showTermExplan(141102,event)" class="ingame-term-desc">Inksplash of Mind</a>, he deals <span class="ingame-Wind">Aero DMG</span> equal to 600% of his ATK to all targets within range, considered Echo Skill DMG.</div>`,
-    hasStacks: false,
-    modifiers: [
       {
         modifier: "EnableAttack",
-        modifierValue: ["SequenceNode6ThusIHeardThusISawThusISpoke"],
+        modifierValue: ["SequenceNode1WanderingThroughtheDesolateCorridors"],
       },
     ],
     minStacks: 0,
@@ -118,14 +19,108 @@ export const resonanceChains = [
     alwaysEnabled: false,
   },
   {
-    key: "SequenceNode6ThusIHeardThusISawThusISpokeStrawCapeinDrizzlyRain",
-    name: "Sequence Node 6: Thus I Heard, Thus I Saw, Thus I Spoke - Straw Cape in Drizzly Rain",
-    details: `<div>Casting Resonance Skill <span class="ingame-Highlight">Straw Cape in Drizzly Rain</span> increases Qiuyuan's Crit. DMG by 100% for 6s. Switching to another Resonator ends this effect early.</div>`,
+    key: "SequenceNode2IntotheWebofEndlessBonds",
+    name: "Sequence Node 2: Into the Web of Endless Bonds",
+    details: `<span class="skilldescription">Ignore 10% of the target's Havoc RES when dealing damage.<br>
+Nearby Resonators in the team with <span class="Highlight">Thread of Bane</span> gain 50% All-Attribute DMG Bonus.</span>`,
     hasStacks: false,
     modifiers: [
       {
-        modifier: "CritDMG",
+        modifier: "ResistShred:Havoc",
+        modifierValue: 0.1,
+      },
+      {
+        modifier: "AllElementAttributeBonus",
+        modifierValue: 0.5,
+      },
+    ],
+    minStacks: 0,
+    maxStacks: 0,
+    alwaysEnabled: false,
+  },
+  {
+    key: "SequenceNode3Across the Confusion of the Long Night",
+    name: "Sequence Node 3: O Blade, I, Who Save No More",
+    details: `<div>The DMG Multipliers of <span class="Highlight"><strong>Sawring - Blitz</strong></span>, <span class="Highlight"><strong>Chainsaw Mode - Dodge Counter</strong></span> and <span class="Highlight"><strong>Sawring - Eradication</strong></span> are increased by 120%. This effect is mutually stackable with that of <span class="Highlight"><strong>Woven Myriad - Convergence</strong></span>.<br>The bonus DMG Multiplier for <span class="Highlight"><strong>Sawring - Eradication</strong></span> granted by <span class="Highlight"><strong>Sawring- Blitz</strong></span> and <span class="Highlight"><strong>Chainsaw Mode - Dodge Counter</strong></span> when <span class="Highlight"><strong>Ring of Chainsaw</strong></span> is consumed is increased by 120%. This effect is mutually stackable with that of <span class="Highlight"><strong>Woven Myriad - Convergence</strong></span>.<br>The Vibration Strength Reduction Rate of <span class="Highlight"><strong>Sawring - Blitz</strong></span>, <span class="Highlight"><strong>Chainsaw Mode - Dodge Counter</strong></span> and <span class="Highlight"><strong>Sawring - Eradication</strong></span> is increased by 50%.</div>`,
+    hasStacks: false,
+    modifiers: [
+      {
+        modifier: "talentModifierMultiply",
+        modifySpecificTalents: [
+          "SawringBlitzStage1DMG",
+          "SawringBlitzStage2DMG",
+          "SawringBlitzStage2HoldDMG",
+          "SawringBlitzStage2DiscordanceDMG",
+          "SawringBlitzStage3DMG",
+          "SawringBlitzStage3HoldDMG",
+          "SawringBlitzStage3FalltoneDMG",
+          "SawringEradicationDMG",
+          "ChainsawModeDodgeCounterDMG",
+          "ChainsawModeDodgeCounterHoldDMG",
+        ],
+        modifierValue: 1.2,
+      },
+    ],
+    minStacks: 0,
+    maxStacks: 0,
+    alwaysEnabled: false,
+  },
+  {
+    key: "SequenceNode4SeveringtheEndlessCycleofTragicFate",
+    name: "Sequence Node 4: Severing the Endless Cycle of Tragic Fate",
+    details: `<span class="skilldescription">The effect of <span class="Highlight">Unseen Snare</span> becomes:<br>
+When targets marked by <span class="Highlight">Unseen Snare</span> take direct damage from Resonators, Chisa inflicts 1 stack of <span class="Highlight">Havoc Bane</span> on them. This effect is triggered up to once every 1s.</span>`,
+    hasStacks: false,
+    modifiers: [],
+    minStacks: 0,
+    maxStacks: 0,
+    alwaysEnabled: false,
+  },
+  {
+    key: "SequenceNode5ThousandsofLightstoGuidetheWayHome",
+    name: "Sequence Node 5: Thousands of Lights to Guide the Way Home",
+    details: `<span class="skilldescription">Resonance Liberation Moment of <span class="Hightlight">Nihility</span> gains 100% DMG Bonus.<br>
+<span class="Highlight">Lifethread - Glide</span> costs 50% less <span class="Highlight">Lifethread - Jetstream</span>.</span>`,
+    hasStacks: false,
+    modifiers: [
+      {
+        modifySpecificTalents: ["MomentofNihilityDMG"],
         modifierValue: 1,
+      },
+    ],
+    minStacks: 0,
+    maxStacks: 0,
+    alwaysEnabled: false,
+  },
+  {
+    key: "SequenceNode6ThusHopeisRekindledwiththeRisingDawn",
+    name: "Sequence Node 6: Thus, Hope is Rekindled with the Rising Dawn",
+    details: `<div">When Chisa takes a fatal blow during <span class="Hightlight"><strong>Sawring - Blitz</strong></span>, <span  class="Hightlight"><strong>Sawring - Eradication</strong></span>, and <span class="Hightlight"><strong>Chainsaw Mode - Dodge Counter</strong></span>, she will remain standing with at least 1 HP.<br><br><span class="Hightlight"><strong>Unseen Snare</strong></span> becomes <span class="Hightlight"><strong>Unseen Snare - Finality</strong></span>, which has the following effects:<br>- <span class="Hightlight"><strong>Unseen Snare - Finality</strong></span> has all the effects of <span class="Hightlight"><strong>Unseen Snare</strong></span>.<br>- Targets affected by <span class="Hightlight""><strong>Unseen Snare - Finality</strong></span> takes 30% Amplified DMG from Negative Statuses.<br>- Targets affected by <span class="Hightlight"><strong>Unseen Snare - Finality</strong></span> takes 40% increased DMG from Chisa.</div>`,
+    hasStacks: false,
+    modifiers: [
+      {
+        modifier: "specialMultiplier",
+        modifierValue: 0.4,
+      },
+      {
+        modifier: "DMGDeepen:SpectroFrazzle",
+        modifierValue: 0.3,
+      },
+      {
+        modifier: "DMGDeepen:AeroErosion",
+        modifierValue: 0.3,
+      },
+      {
+        modifier: "DMGDeepen:ElectroFlare",
+        modifierValue: 0.3,
+      },
+      {
+        modifier: "DMGDeepen:FusionBurst",
+        modifierValue: 0.3,
+      },
+      {
+        modifier: "DMGDeepen:GlacioChafe",
+        modifierValue: 0.3,
       },
     ],
     minStacks: 0,
