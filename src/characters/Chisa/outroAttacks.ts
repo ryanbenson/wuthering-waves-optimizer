9a786b34cc1ee4cf3abe25148fd455cd3ef9e213
Ryan Benson <benson.ryan@gmail.com>
2025-11-19 10:25:03 -0600

    Removes strongs

--- src/characters/Chisa/outroAttacks.ts
+++ src/characters/Chisa/outroAttacks.ts
@@ -1,5 +1,5 @@
 export const outroAttacks = {
   name: "Outro Skill: Unraveling - Law Zero",
-  description: `<div>Grant <span class="Highlight"><strong>Resonant Thread of Closure</strong></span> to all nearby Resonators in the team for 20s.<br>While in <span class="Highlight"><strong>Resonant Thread of Closure</strong></span>:<br>- When an attack hits, increase the max stacks of <span class="Highlight"><strong>Negative Status DMG</strong></span> grants <span class="Highlight"><strong>Thread of Bane</strong></span> for 15s.</div>`,
+  description: `<div>Grant <span class="Highlight">Resonant Thread of Closure</span> to all nearby Resonators in the team for 20s.<br>While in <span class="Highlight">Resonant Thread of Closure</span>:<br>- When an attack hits, increase the max stacks of <span class="Highlight">Negative Status DMG</span> grants <span class="Highlight">Thread of Bane</span> for 15s.</div>`,
   attacks: [],
 };
