03fce5a47e30058c5f6c284fbd79b49820dfa462
Ryan Benson <benson.ryan@gmail.com>
2025-12-30 07:57:05 -0600

    Updates lynae s6

--- src/pages/UpdatesView.vue
+++ src/pages/UpdatesView.vue
@@ -2,6 +2,13 @@
   <Nav cur-page="updates" :disable-mobile-nav="true"></Nav>
   <article class="prose page-updates">
     <h1>Updates</h1>
+    <h3>December 30, 2025</h3>
+    <ul>
+      <li>
+        Updates Lynae s6 text and modifier, it is now Vulnerability due to the
+        text changes made.
+      </li>
+    </ul>
     <h3>December 29, 2025</h3>
     <ul>
       <li>Adds missing Halo of Starry Radiance to team buffs.</li>
