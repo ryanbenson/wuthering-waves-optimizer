5f849993b20a6869166274061c8eab689dfd7c89
Ryan Benson <benson.ryan@gmail.com>
2026-01-06 12:57:49 -0600

    Adds galbrena test

---
+++ cypress/e2e/calculator/data/Galbrena/data.ts
@@ -0,0 +1,236 @@
+export const config = {
+  meta: { version: "2", source: "WutheringTools" },
+  data: {
+    character:
+      '{"characters":{"Augusta":{"echoes":{"0":{},"1":{},"2":{},"3":{},"4":{}},"mainEcho":{"echo":null,"rank":5},"optimizer":{"mainEchoes":[],"echoSets":[],"minStats":[],"optimizationTarget":[],"damageType":"Average","ignoreOtherResonantorEchoes":false}},"Galbrena":{"echoes":{"0":{"type":null,"echo":null,"echoSet":null,"stat":null,"echoSubStatsType1":null,"echoSubStatsValue1":null,"echoSubStatsType2":null,"echoSubStatsValue2":null,"echoSubStatsType3":null,"echoSubStatsValue3":null,"echoSubStatsType4":null,"echoSubStatsValue4":null,"echoSubStatsType5":null,"echoSubStatsValue5":null,"rank":null,"echoId":"9819cdpsri"},"1":{"type":null,"echo":null,"echoSet":null,"stat":null,"echoSubStatsType1":null,"echoSubStatsValue1":null,"echoSubStatsType2":null,"echoSubStatsValue2":null,"echoSubStatsType3":null,"echoSubStatsValue3":null,"echoSubStatsType4":null,"echoSubStatsValue4":null,"echoSubStatsType5":null,"echoSubStatsValue5":null,"rank":null,"echoId":"8xziafu5n3"},"2":{"type":null,"echo":null,"echoSet":null,"stat":null,"echoSubStatsType1":null,"echoSubStatsValue1":null,"echoSubStatsType2":null,"echoSubStatsValue2":null,"echoSubStatsType3":null,"echoSubStatsValue3":null,"echoSubStatsType4":null,"echoSubStatsValue4":null,"echoSubStatsType5":null,"echoSubStatsValue5":null,"rank":null,"echoId":"5vtv4cky74"},"3":{"type":null,"echo":null,"echoSet":null,"stat":null,"echoSubStatsType1":null,"echoSubStatsValue1":null,"echoSubStatsType2":null,"echoSubStatsValue2":null,"echoSubStatsType3":null,"echoSubStatsValue3":null,"echoSubStatsType4":null,"echoSubStatsValue4":null,"echoSubStatsType5":null,"echoSubStatsValue5":null,"rank":null,"echoId":"q20ig2ty3n"},"4":{"type":null,"echo":null,"echoSet":null,"stat":null,"echoSubStatsType1":null,"echoSubStatsValue1":null,"echoSubStatsType2":null,"echoSubStatsValue2":null,"echoSubStatsType3":null,"echoSubStatsValue3":null,"echoSubStatsType4":null,"echoSubStatsValue4":null,"echoSubStatsType5":null,"echoSubStatsValue5":null,"rank":null,"echoId":"e5ozbynu4w"}},"mainEcho":{"echo":"Corrosaurus","rank":5,"isEnabled":true},"optimizer":{"mainEchoes":[],"echoSets":[],"minStats":[],"optimizationTarget":[],"damageType":"Average","ignoreOtherResonantorEchoes":false},"buffs":{"Afterflame":{"isEnabled":true,"stacks":40},"BurningDrive":{"isEnabled":true},"HellfireAbsolution":{"isEnabled":true},"InherentSkillOathboundHunt":{"isEnabled":true,"stacks":4},"InherentSkillSinFeaster":{"isEnabled":true},"StatBonusCritDMG1":{"isEnabled":true},"StatBonusCritDMG2":{"isEnabled":true},"StatBonusCritDMG3":{"isEnabled":true},"StatBonusCritDMG4":{"isEnabled":true},"StatBonusATK1":{"isEnabled":true},"StatBonusATK2":{"isEnabled":true},"StatBonusATK3":{"isEnabled":true},"StatBonusATK4":{"isEnabled":true}},"weapon":"LuxUmbra","weaponPassives":{"ToFireSheReturnsATK":{"isEnabled":true},"ToFireSheReturnsDmgDeepenHeavy":{"isEnabled":true},"ToFireSheReturnsDmgDeepenEcho":{"isEnabled":true},"ToFireSheReturnsDefIgnore":{"isEnabled":true}},"echoSetBonus":{"setBonusOne":"Molten Rift 2 Set","setBonusTwo":"Flamewing\'s Shadow 3 Set"},"echoSetPassives":{"MoltenRift2SetFusion":{"isEnabled":true},"FlamewingsShadowCritRateHeavy":{"isEnabled":true},"FlamewingsShadowCritRateEcho":{"isEnabled":true},"FlamewingsShadowFusion":{"isEnabled":true}},"resonanceChains":{"SequenceNode1HeartofDefianceEverAblaze":{"isEnabled":true},"SequenceNode2HellboundDiveofFireandAbyss":{"isEnabled":true},"SequenceNode3HuntersBloodOathRekindled":{"isEnabled":true},"SequenceNode4CarryForthThisFadingSpark":{"isEnabled":true},"SequenceNode5ThoughLightFadesTormentConsumes":{"isEnabled":true},"SequenceNode6IRemainWhoIamEternalMyFlame":{"isEnabled":true}},"teamBuffs":{"selectedCharacter1":"Qiuyuan","selectedCharacter2":"Shorekeeper","buffs":{"BamboosShade":{"isEnabled":true},"SunderingStrike":{"isEnabled":true,"baseAttrValue":100},"OutroSkillStrikeBeforeReady":{"isEnabled":true},"SequenceNode2OBladeIWhoTeachNoMore":{"isEnabled":true},"SophisticatedStellarealmCritRate":{"isEnabled":true,"baseAttrValue":250},"ReleasedStellarealmCritDMG":{"isEnabled":true,"baseAttrValue":250},"OutroSkillBinaryButterfly":{"isEnabled":true},"InherentSkillSelfGravitation":{"isEnabled":true},"SequenceNode2NightsGiftandRefusal":{"isEnabled":true},"RejuvenatingGlow":{"isEnabled":true},"FallacyOfNoReturn":{"isEnabled":true},"LawofHarmony":{"isEnabled":true,"stacks":4},"StellarSymphonyATK":{"isEnabled":true},"EmeraldSentenceEchoDMGBonus":{"isEnabled":true}}},"rotations":[{"id":"yvcq7upcof","name":"Galbrena","description":"","duration":null,"echo":"Corrosaurus","echoRank":5,"actions":[{"id":"tyezbyr5wa","order":1,"key":"HellflareOverloadDMG","type":"intro","count":1,"buffs":[{"id":"dw7kb3an8t","modifier":"CritDMG","modifierValue":"100"}],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false},{"id":"w56w8co4w0","order":2,"key":"HeavyAttackVolleyofDeathStage2DMG","type":"basic","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":true,"excludeWeaponBuffs":true,"isDisabled":false},{"order":3,"key":"HeavyAttackVolleyofDeathStage3DMG","type":"basic","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false,"id":"7ef2qcus9a"},{"order":4,"key":"ResonanceSkillEncroachDMG","type":"skill","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false,"id":"lkiwp4p8qa"},{"order":5,"key":"BasicAttackSeraphicExecutionStage2DMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false,"id":"wcjihl58f1"},{"order":6,"key":"BasicAttackStage3DMG","type":"basic","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false,"id":"vjnbd1ygif"},{"order":7,"key":"BasicAttackStage4DMG","type":"basic","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false,"id":"ihuea3pwxs"},{"order":8,"key":"ResonanceSkillRavageDMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false,"id":"erro70r9fi"},{"order":9,"key":"HellfireAbsolutionDMG","type":"liberation","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false,"id":"unvytbptqg"},{"order":10,"key":"BasicAttackSeraphicExecutionStage2DMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false,"id":"x06h6kj45t"},{"order":11,"key":"BasicAttackSeraphicExecutionStage3DMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false,"id":"8u5rexu0g6"},{"order":12,"key":"BasicAttackSeraphicExecutionStage4DMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false,"id":"dsm2efkhe1"},{"order":13,"key":"BasicAttackSeraphicExecutionStage5DMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false,"id":"eaz5qb1rer"},{"order":14,"key":"HellstrideDMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false,"id":"bw108uwq2h"},{"order":15,"key":"BasicAttackSeraphicExecutionStage3DMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false,"id":"svgmzkpymy"},{"order":16,"key":"BasicAttackSeraphicExecutionStage4DMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false,"id":"frjxoi69h7"},{"order":17,"key":"BasicAttackSeraphicExecutionStage5DMG","type":"forteCircuit","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false,"id":"rdjpxfz080"},{"order":18,"key":"AshenPursuitDMG","type":"outro","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false,"id":"gsuwojd56i"},{"id":"yy0m3y01ij","order":19,"key":"CorrosaurusAttack","type":"echoAttacks","count":1,"buffs":[],"excludeSelfBuffs":false,"excludeTeamBuffs":false,"excludeWeaponBuffs":false,"isDisabled":false,"mainEcho":"Corrosaurus","mainEchoRank":5}]}],"customBuffs":{"ATK":2,"ATK_FLAT":2,"HP":2,"HP_FLAT":2,"DEF":2,"DEF_FLAT":2,"CritRate":2,"CritDMG":2,"EnergyRegen":2,"BasicAttackDMGBonus":2,"HeavyAttackDMGBonus":2,"ResonanceSkillDMGBonus":2,"ResonanceLiberationDMGBonus":2,"Glacio":2,"Fusion":2,"Electro":2,"Aero":2,"Spectro":2,"Havoc":2,"HealingBonus":2,"CoordinatedDMGBonus":2,"EchoDMGBonus":2,"TuneBreakDMGBonus":2,"DamageAmplify":2,"ResistShred":2,"DefIgnore":2,"DefReduction":2,"SpecialMultiplier":2},"havocBaneStacks":"6"}},"activeCharacter":"Galbrena"}',
+    inventory:
+      '{"echoes":[{"echoId":"9819cdpsri","echo":"Corrosaurus","echoSet":"FlamewingsShadow","echoSubStatsType1":"CritRate","echoSubStatsType2":"CritDMG","echoSubStatsType3":"ATK","echoSubStatsType4":"ATK_FLAT","echoSubStatsType5":"HeavyAttackDMGBonus","echoSubStatsValue1":8.1,"echoSubStatsValue2":16.2,"echoSubStatsValue3":8.6,"echoSubStatsValue4":50,"echoSubStatsValue5":8.6,"rank":5,"stat":"Fusion","type":3},{"echoId":"8xziafu5n3","echo":"ReminiscenceThrenodianLeviathan","echoSet":"FlamewingsShadow","echoSubStatsType1":"CritRate","echoSubStatsType2":"CritDMG","echoSubStatsType3":"ATK","echoSubStatsType4":"ATK_FLAT","echoSubStatsType5":"HeavyAttackDMGBonus","echoSubStatsValue1":8.1,"echoSubStatsValue2":16.2,"echoSubStatsValue3":8.6,"echoSubStatsValue4":50,"echoSubStatsValue5":8.6,"rank":5,"stat":"CritRate","type":4},{"echoId":"5vtv4cky74","echo":"NimbusWraith","echoSet":"FlamewingsShadow","echoSubStatsType1":"CritRate","echoSubStatsType2":"CritDMG","echoSubStatsType3":"EnergyRegen","echoSubStatsType4":"HP","echoSubStatsType5":"DEF","echoSubStatsValue1":8.1,"echoSubStatsValue2":16.2,"echoSubStatsValue3":9.2,"echoSubStatsValue4":8.6,"echoSubStatsValue5":10.9,"rank":5,"stat":"ATK","type":1},{"echoId":"q20ig2ty3n","echo":"VioletFeatheredHeron","echoSet":"MoltenRift","echoSubStatsType1":"CritRate","echoSubStatsType2":"CritDMG","echoSubStatsType3":"EnergyRegen","echoSubStatsType4":"HP","echoSubStatsType5":"DEF","echoSubStatsValue1":8.1,"echoSubStatsValue2":16.2,"echoSubStatsValue3":9.2,"echoSubStatsValue4":8.6,"echoSubStatsValue5":10.9,"rank":5,"stat":"Fusion","type":3},{"echoId":"e5ozbynu4w","echo":"SnipSnap","echoSet":"MoltenRift","echoSubStatsType1":"CritRate","echoSubStatsType2":"CritDMG","echoSubStatsType3":"EnergyRegen","echoSubStatsType4":"HP","echoSubStatsType5":"DEF","echoSubStatsValue1":8.1,"echoSubStatsValue2":16.2,"echoSubStatsValue3":9.2,"echoSubStatsValue4":8.6,"echoSubStatsValue5":10.9,"rank":5,"stat":"ATK","type":1},{"echo":"ReminiscenceThrenodianLeviathan","type":4,"rank":5,"stat":"CritRate","echoId":"qy27jq1l5x","echoSet":"FlamewingsShadow","echoSubStatsType1":"CritRate","echoSubStatsValue1":6.3,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":12.6,"echoSubStatsType3":"ATK_FLAT","echoSubStatsValue3":30,"echoSubStatsType4":"ATK","echoSubStatsValue4":6.4,"echoSubStatsType5":"HP","echoSubStatsValue5":6.4},{"echo":"Corrosaurus","type":3,"rank":5,"stat":"Fusion","echoId":"qag163ys2f","echoSet":"FlamewingsShadow","echoSubStatsType1":"CritRate","echoSubStatsValue1":10.5,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":21,"echoSubStatsType3":"ATK","echoSubStatsValue3":11.6,"echoSubStatsType4":"ATK_FLAT","echoSubStatsValue4":60,"echoSubStatsType5":"HP","echoSubStatsValue5":11.6},{"echo":"NightmareBabyViridblazeSaurian","type":1,"rank":5,"stat":"ATK","echoId":"28241qom1b","echoSet":"FlamewingsShadow","echoSubStatsType1":"CritRate","echoSubStatsValue1":6.3,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":12.6,"echoSubStatsType3":"ATK","echoSubStatsValue3":6.4,"echoSubStatsType4":"ATK_FLAT","echoSubStatsValue4":30,"echoSubStatsType5":"HP","echoSubStatsValue5":6.4},{"echo":"NimbusWraith","type":1,"rank":5,"stat":"ATK","echoId":"tmm72b568p","echoSet":"FlamewingsShadow","echoSubStatsType1":"CritRate","echoSubStatsValue1":10.5,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":21,"echoSubStatsType3":"ATK","echoSubStatsValue3":11.6,"echoSubStatsType4":"ATK_FLAT","echoSubStatsValue4":60,"echoSubStatsType5":"HP","echoSubStatsValue5":11.6}],"equipped":{"9819cdpsri":{"Galbrena":0},"8xziafu5n3":{"Galbrena":1},"5vtv4cky74":{"Galbrena":2},"q20ig2ty3n":{"Galbrena":3},"e5ozbynu4w":{"Galbrena":4}},"echoPresets":[],"equippedPresets":{}}',
+  },
+};
+
+export const dataAttacks: AttackTests = [
+  {
+    selector: ".basic-attack-stage-1-dmg",
+    values: ["Basic Attack Stage 1 DMG", "4960", "17489", "17489"],
+  },
+  {
+    selector: ".basic-attack-stage-2-dmg",
+    values: ["Basic Attack Stage 2 DMG", "11024", "38869", "38869"],
+  },
+  {
+    selector: ".basic-attack-stage-3-dmg",
+    values: ["Basic Attack Stage 3 DMG", "11984", "42253", "42253"],
+  },
+  {
+    selector: ".basic-attack-stage-4-dmg",
+    values: ["Basic Attack Stage 4 DMG", "28732", "101306", "101306"],
+  },
+  {
+    selector: ".dodge-counter-blood-for-blood-dmg",
+    values: ["Dodge Counter - Blood for Blood DMG", "17202", "60651", "60651"],
+  },
+  {
+    selector: ".mid-air-attack-ashfall-barrage-plunging-attack-dmg",
+    values: [
+      "Mid-air Attack - Ashfall Barrage Plunging Attack DMG",
+      "11998",
+      "42303",
+      "42303",
+    ],
+  },
+  {
+    selector: ".mid-air-attack-ashfall-barrage-sustained-fire-dmg",
+    values: [
+      "Mid-air Attack - Ashfall Barrage Sustained Fire DMG",
+      "2250",
+      "7932",
+      "7932",
+    ],
+  },
+  {
+    selector: ".heavy-attack-volley-of-death-stage-1-dmg",
+    values: [
+      "Heavy Attack - Volley of Death Stage 1 DMG",
+      "17220",
+      "60718",
+      "60718",
+    ],
+  },
+  {
+    selector: ".heavy-attack-volley-of-death-stage-2-dmg",
+    values: [
+      "Heavy Attack - Volley of Death Stage 2 DMG",
+      "11176",
+      "39404",
+      "39404",
+    ],
+  },
+  {
+    selector: ".heavy-attack-volley-of-death-stage-3-dmg",
+    values: [
+      "Heavy Attack - Volley of Death Stage 3 DMG",
+      "27090",
+      "95519",
+      "95519",
+    ],
+  },
+  {
+    selector: ".resonance-skill-encroach-dmg",
+    values: ["Resonance Skill - Encroach DMG", "7497", "26434", "26434"],
+  },
+  {
+    selector: ".resonance-skill-ascent-of-malice-dmg",
+    values: [
+      "Resonance Skill - Ascent of Malice DMG",
+      "21611",
+      "76198",
+      "76198",
+    ],
+  },
+  {
+    selector: ".hellfire-absolution-dmg",
+    values: ["Hellfire Absolution DMG", "412049", "1452884", "1452884"],
+  },
+  {
+    selector: ".hellstride-dmg",
+    values: ["Hellstride DMG", "666", "666", "666"],
+  },
+  {
+    selector: ".basic-attack-seraphic-execution-stage-1-dmg",
+    values: [
+      "Basic Attack - Seraphic Execution Stage 1 DMG",
+      "23420",
+      "101314",
+      "101314",
+    ],
+  },
+  {
+    selector: ".basic-attack-seraphic-execution-stage-2-dmg",
+    values: [
+      "Basic Attack - Seraphic Execution Stage 2 DMG",
+      "55261",
+      "239056",
+      "239056",
+    ],
+  },
+  {
+    selector: ".basic-attack-seraphic-execution-stage-3-dmg",
+    values: [
+      "Basic Attack - Seraphic Execution Stage 3 DMG",
+      "96542",
+      "417639",
+      "417639",
+    ],
+  },
+  {
+    selector: ".basic-attack-seraphic-execution-stage-4-dmg",
+    values: [
+      "Basic Attack - Seraphic Execution Stage 4 DMG",
+      "130633",
+      "565117",
+      "565117",
+    ],
+  },
+  {
+    selector: ".basic-attack-seraphic-execution-stage-5-dmg",
+    values: [
+      "Basic Attack - Seraphic Execution Stage 5 DMG",
+      "161443",
+      "698400",
+      "698400",
+    ],
+  },
+  {
+    selector: ".heavy-attack-flamewing-verdict-stage-1-dmg",
+    values: [
+      "Heavy Attack - Flamewing Verdict Stage 1 DMG",
+      "47023",
+      "203418",
+      "203418",
+    ],
+  },
+  {
+    selector: ".heavy-attack-flamewing-verdict-stage-2-dmg",
+    values: [
+      "Heavy Attack - Flamewing Verdict Stage 2 DMG",
+      "30451",
+      "131731",
+      "131731",
+    ],
+  },
+  {
+    selector: ".heavy-attack-flamewing-verdict-stage-3-dmg",
+    values: [
+      "Heavy Attack - Flamewing Verdict Stage 3 DMG",
+      "127300",
+      "550698",
+      "550698",
+    ],
+  },
+  {
+    selector: ".dodge-counter-purgatory-scourge-dmg",
+    values: [
+      "Dodge Counter - Purgatory Scourge DMG",
+      "127441",
+      "551310",
+      "551310",
+    ],
+  },
+  {
+    selector: ".mid-air-attack-hellsent-barrage-plunging-attack-dmg",
+    values: [
+      "Mid-air Attack - Hellsent Barrage Plunging Attack DMG",
+      "63145",
+      "273165",
+      "273165",
+    ],
+  },
+  {
+    selector: ".mid-air-attack-hellsent-barrage-sustained-fire-dmg",
+    values: [
+      "Mid-air Attack - Hellsent Barrage Sustained Fire DMG",
+      "11843",
+      "51233",
+      "51233",
+    ],
+  },
+  {
+    selector: ".resonance-skill-ravage-dmg",
+    values: ["Resonance Skill - Ravage DMG", "14495", "62706", "62706"],
+  },
+  {
+    selector: ".hellflare-overload-dmg",
+    values: ["Hellflare Overload DMG", "6174", "18961", "21768"],
+  },
+  {
+    selector: ".ashen-pursuit-dmg",
+    values: ["Ashen Pursuit DMG", "52146", "160156", "183865"],
+  },
+  {
+    selector: ".tune-break-dmg",
+    values: ["Tune Break DMG", "77709", "77709", "77709"],
+  },
+  {
+    selector: ".attacks-dmg",
+    values: ["Attacks DMG", "44197", "155838", "155838"],
+  },
+];
+export const dataStats: StatTests = [
+  { selector: ".stat-atk", value: "4,104" },
+  { selector: ".stat-hp", value: "17.725" },
+  { selector: ".stat-def", value: "1,499" },
+  { selector: ".stat-cr", value: "82.0%" },
+  { selector: ".stat-cd", value: "352.6%" },
+  { selector: ".stat-er", value: "129.6%" },
+  { selector: ".stat-basic", value: "2.0%" },
+  { selector: ".stat-heavy", value: "19.2%" },
+  { selector: ".stat-skill", value: "2.0%" },
+  { selector: ".stat-liberation", value: "2.0%" },
+  { selector: ".stat-glacio", value: "22.0%" },
+  { selector: ".stat-fusion", value: "120.0%" },
+  { selector: ".stat-electro", value: "22.0%" },
+  { selector: ".stat-aero", value: "22.0%" },
+  { selector: ".stat-spectro", value: "22.0%" },
+  { selector: ".stat-havoc", value: "22.0%" },
+  { selector: ".stat-healing", value: "2.0%" },
+];
