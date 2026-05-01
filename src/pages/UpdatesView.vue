a8ba5ecf0044cb3020bfc556d884a2f626fbe070
Ryan Benson <benson.ryan@gmail.com>
2026-05-01 08:56:31 -0500

    Fixes hiyuki forte 1 talent

--- src/pages/UpdatesView.vue
+++ src/pages/UpdatesView.vue
@@ -2,6 +2,10 @@
   <Nav cur-page="updates" :disable-mobile-nav="true"></Nav>
   <article class="prose page-updates">
     <h1>Updates</h1>
+    <h3>May 1, 2026</h3>
+    <ul>
+      <li>Fixes Hiyuki forte 1 on her Iai attack.</li>
+    </ul>
     <h3>April 30, 2026</h3>
     <ul>
       <li>Fixes text issue on Hiyuki intro skill.</li>
