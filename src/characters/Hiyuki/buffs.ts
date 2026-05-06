db860e841c02c2334fc14e07825ee6b8df2609ae
Ryan Benson <benson.ryan@gmail.com>
2026-05-06 13:57:14 -0500

    Updates snowforged to be multiply not additive to mv

--- src/characters/Hiyuki/buffs.ts
+++ src/characters/Hiyuki/buffs.ts
@@ -9,6 +9,7 @@ export const buffs = [
       {
         modifier: "Talent",
         modifierTalentKey: "ForeclaimingBladeLiberationBaseDMG",
+        modifierTalentTarget: "talentModifierMultiply",
         modifierValue: {
           "1": 4,
           "2": 4.328,
