2c480188ae20d4ae6c4c6b4e1afb47f03f9376f0
Ryan Benson <benson.ryan@gmail.com>
2026-05-21 09:13:42 -0500

    Adds notice for denia

--- src/pages/UpdatesView.vue
+++ src/pages/UpdatesView.vue
@@ -2,6 +2,10 @@
   <Nav cur-page="updates" :disable-mobile-nav="true"></Nav>
   <article class="prose page-updates">
     <h1>Updates</h1>
+    <h3>May 21, 2026</h3>
+    <ul>
+      <li>Adds Denia and her sig.</li>
+    </ul>
     <h3>May 17, 2026</h3>
     <ul>
       <li>Adds negative status amplify as custom buffs and action-level buffs in rotations.</li>
