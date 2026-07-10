5d0b73b5924be8589cf7456fee0f494243a90b3e
Ryan Benson <benson.ryan@gmail.com>
2026-07-10 13:02:17 -0500

    chore: augusta echo preset generated

--- src/characters/Augusta/presets.ts
+++ src/characters/Augusta/presets.ts
@@ -7,4 +7,11 @@ export const rotations: RotationPreset[] = [
     },
 ];
 
-export const echoes = [];
+export const echoes = [
+    {
+        name: `Standard CD Build`,
+        description: `43311 CD / 2x Electro Crown of Valor + Void Thunder. 40.5% CR, 81% CD, 17.2% ATK, 17.2% Heavy, 100 ATK, 27.6% ER`,
+        author: `thundertooth`,
+        data: {"echoes":{"0":{"type":4,"echo":"TheFalseSovereign","echoSet":"CrownofValor","stat":"CritDMG","echoSubStatsType1":"CritRate","echoSubStatsValue1":8.1,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":16.2,"echoSubStatsType3":"ATK","echoSubStatsValue3":8.6,"echoSubStatsType4":"ATK_FLAT","echoSubStatsValue4":50,"echoSubStatsType5":"HeavyAttackDMGBonus","echoSubStatsValue5":8.6},"1":{"type":3,"echo":"NightmareVioletFeatheredHeron","echoSet":"CrownofValor","stat":"Electro","echoSubStatsType1":"CritRate","echoSubStatsValue1":8.1,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":16.2,"echoSubStatsType3":"ATK","echoSubStatsValue3":8.6,"echoSubStatsType4":"ATK_FLAT","echoSubStatsValue4":50,"echoSubStatsType5":"HeavyAttackDMGBonus","echoSubStatsValue5":8.6},"2":{"type":3,"echo":"Hurriclaw","echoSet":"CrownofValor","stat":"Electro","echoSubStatsType1":"CritRate","echoSubStatsValue1":8.1,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":16.2,"echoSubStatsType3":"EnergyRegen","echoSubStatsValue3":9.2,"echoSubStatsType4":"DEF_FLAT","echoSubStatsValue4":50,"echoSubStatsType5":"DEF","echoSubStatsValue5":10.9},"3":{"type":1,"echo":"TrafficIlluminator","echoSet":"VoidThunder","stat":"ATK","echoSubStatsType1":"CritRate","echoSubStatsValue1":8.1,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":16.2,"echoSubStatsType3":"EnergyRegen","echoSubStatsValue3":9.2,"echoSubStatsType4":"DEF","echoSubStatsValue4":10.9,"echoSubStatsType5":"HP_FLAT","echoSubStatsValue5":50},"4":{"type":1,"echo":"FusionWarrior","echoSet":"VoidThunder","stat":"ATK","echoSubStatsType1":"CritRate","echoSubStatsValue1":8.1,"echoSubStatsType2":"CritDMG","echoSubStatsValue2":16.2,"echoSubStatsType3":"EnergyRegen","echoSubStatsValue3":9.2,"echoSubStatsType4":"DEF_FLAT","echoSubStatsValue4":50,"echoSubStatsType5":"HP","echoSubStatsValue5":8.6}},"mainEcho":{"echo":"TheFalseSovereign","rank":5}}
+    }
+];
