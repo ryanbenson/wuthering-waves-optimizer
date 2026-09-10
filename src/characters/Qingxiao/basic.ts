7531432d543c9b0daa3aedc2e937192f7ca84226
Ryan Benson <benson.ryan@gmail.com>
2026-09-10 14:54:32 -0500

    Add suggested weapons for Qingxiao character

--- src/characters/Qingxiao/basic.ts
+++ src/characters/Qingxiao/basic.ts
@@ -11,6 +11,11 @@ export function getCharacterBasicInfo(): CharacterBasicInfo {
     inherentSkillIcons: ["https://api.encore.moe/resource/Data/Game/Aki/UI/UIResources/Common/Atlas/SkillIcon/SkillIconQingxiao/SP_IconQingxiaoD1.webp","https://api.encore.moe/resource/Data/Game/Aki/UI/UIResources/Common/Atlas/SkillIcon/SkillIconQingxiao/SP_IconQingxiaoD2.webp"],
     signatureWeapon: "GlintOfClouds",
     liveResultBarStats: ["totalAtk", "totalCritRate", "totalCritDMG", "energyRegen"],
-    suggestedWeapons: [],
+    suggestedWeapons: [
+      { key: "RedSpring", label: "Alternate" },
+      { key: "EmeraldofGenesis", label: "Standard" },
+      { key: "FeatherEdge", label: "BP" },
+      { key: "CommandoofConviction", label: "4*" },
+    ],
   };
 }
