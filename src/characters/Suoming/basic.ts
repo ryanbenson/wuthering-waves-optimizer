f8f77b1b55eb71d96c67008f53e29ff3ea783d23
Ryan Benson <benson.ryan@gmail.com>
2026-09-20 10:39:05 -0500

    feat: fixes some Suoming kit

--- src/characters/Suoming/basic.ts
+++ src/characters/Suoming/basic.ts
@@ -4,9 +4,12 @@ export function getCharacterBasicInfo(): CharacterBasicInfo {
     rarity: 5,
     weapon: "Swords",
     avatarUrl: "Suoming.png",
-    gender: "unknown",
+    gender: "female",
     element: "Electro",
     image: "https://api.encore.moe/resource/Data/Game/Aki/UI/UIResources/Common/Image/IconRolePile/T_IconRole_Pile_suoming_UI.webp",
     inherentSkillIcons: ["https://api.encore.moe/resource/Data/Game/Aki/UI/UIResources/Common/Atlas/SkillIcon/SkillIconSuoming/SP_IconSuomingD1.webp","https://api.encore.moe/resource/Data/Game/Aki/UI/UIResources/Common/Atlas/SkillIcon/SkillIconSuoming/SP_IconSuomingD2.webp"],
+    signatureWeapon: "UnspokenRue",
+    liveResultBarStats: ["totalAtk", "totalCritRate", "totalCritDMG", "energyRegen"],
+    suggestedWeapons: [],
   };
 }
